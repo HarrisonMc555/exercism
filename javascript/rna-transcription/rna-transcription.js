2aacba97988a1055aa6166bf3570e758d52337e3
Harrison McCullough <mccullough.harrison@gmail.com>
2017-04-17 23:49:56 -0600

    Finished javascript/rna-transcription.

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,17 @@
+const DnaTranscriber = function DnaTranscriber() {};
+
+DnaTranscriber.prototype.convertBase = function convertBase(b) {
+  switch (b) {
+    case 'G' : return 'C';
+    case 'C' : return 'G';
+    case 'T' : return 'A';
+    case 'A' : return 'U';
+    default : throw Error('Invalid input');
+  }
+};
+
+DnaTranscriber.prototype.toRna = function toRna(seq) {
+  return Array.prototype.map.call(seq, e => this.convertBase(e)).join('');
+};
+
+module.exports = DnaTranscriber;
