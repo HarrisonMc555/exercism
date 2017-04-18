9a3f56a9df748aa36d7f1a1afd6f07ce02e85796
Harrison McCullough <mccullough.harrison@gmail.com>
2017-04-17 23:50:20 -0600

    Finished javascript/pangram.

---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,27 @@
+const Pangram = function Pangram(input) {
+  this.text = input;
+};
+
+Pangram.prototype.alphabet = (function createAlphabet() {
+  'use strict';
+
+  const result = [];
+  for (let i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i += 1) {
+    result.push(String.fromCharCode(i));
+  }
+  return result;
+}());
+
+Pangram.prototype.strContain = function strContain(s, c) {
+  return s.indexOf(c) > -1;
+};
+
+Pangram.prototype.isPangram = function isPangram() {
+  'use strict';
+
+  if (!this.text) { return false; }
+  const s = this.text.toLowerCase();
+  return this.alphabet.every(c => this.strContain(s, c));
+};
+
+module.exports = Pangram;
