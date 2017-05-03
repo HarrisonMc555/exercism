1cd20242fa9ca773f49feeee51f2aec27fc16bf8
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-02 22:55:12 -0500

    Finished javascript/bob.

---
+++ javascript/bob/bob.js
@@ -0,0 +1,25 @@
+const Bob = function Bob() {};
+
+/* Mimic default implementation of Python's strip */
+function strip(s) {
+  /* Strip leading and trailing spaces */
+  return s.replace(/^ */,'').replace(/ *$/,'');
+}
+
+Bob.prototype.hey = function hey(msg) {
+  if (!strip(msg)) {
+    /* Blank and empty strings */
+    return 'Fine. Be that way!';
+  } else if (msg === msg.toUpperCase() && msg.match(/[a-z]/i)) {
+    /* At least one letter, already all upper case */
+    return 'Whoa, chill out!';
+  } else if (msg[msg.length-1] === '?') {
+    /* Last character is question mark */
+    return 'Sure.'
+  } else {
+    /* Default */
+    return 'Whatever.';
+  }
+};
+
+module.exports = Bob;
