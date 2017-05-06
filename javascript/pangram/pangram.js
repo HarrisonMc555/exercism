49d39f7bebdeb0aa438d7ae05174a1aa79c0a222
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-06 10:08:26 -0500

    Cleaned pangram/pangram.js.

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -4,6 +4,15 @@ const Pangram = function Pangram(input) {
   this.text = input;
 };
 
+/* Return true if the Pangram object is a pangram, i.e. contains every letter in
+   the alphabet */
+Pangram.prototype.isPangram = function isPangram() {
+  if (!this.text) { return false; }
+  const s = this.text.toLowerCase();
+  return this.alphabet.every(c => this.strContain(s, c));
+};
+
+/* Create an array with all lower case letters */
 Pangram.prototype.alphabet = (function createAlphabet() {
   var result = [];
   for (let i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i += 1) {
@@ -12,14 +21,9 @@ Pangram.prototype.alphabet = (function createAlphabet() {
   return result;
 }());
 
+/* Return true if s contains c */
 Pangram.prototype.strContain = function strContain(s, c) {
   return s.indexOf(c) > -1;
-};
-
-Pangram.prototype.isPangram = function isPangram() {
-  if (!this.text) { return false; }
-  const s = this.text.toLowerCase();
-  return this.alphabet.every(c => this.strContain(s, c));
 };
 
 module.exports = Pangram;
