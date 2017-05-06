2baeb8d988882111f72c0e8e3513ac384c962013
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-06 14:45:55 -0500

    Cleaned javascript/food-chain.

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -5,44 +5,44 @@ const FoodChain = function FoodChain() {};
 /* Text for the verses
    [<animal>, <comment>[, <animal to catch>]] */
 FoodChain.prototype.text = [
-  ['fly', 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.'],
-  ['spider', 'It wriggled and jiggled and tickled inside her.', 'spider that wriggled and jiggled and tickled inside her'],
-  ['bird', 'How absurd to swallow a bird!'],
-  ['cat', 'Imagine that, to swallow a cat!'],
-  ['dog', 'What a hog, to swallow a dog!'],
-  ['goat', 'Just opened her throat and swallowed a goat!'],
-  ['cow', 'I don\'t know how she swallowed a cow!'],
-  ['horse', 'She\'s dead, of course!']
+  ['fly', 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n'],
+  ['spider', 'It wriggled and jiggled and tickled inside her.\n', 'spider that wriggled and jiggled and tickled inside her'],
+  ['bird', 'How absurd to swallow a bird!\n'],
+  ['cat', 'Imagine that, to swallow a cat!\n'],
+  ['dog', 'What a hog, to swallow a dog!\n'],
+  ['goat', 'Just opened her throat and swallowed a goat!\n'],
+  ['cow', 'I don\'t know how she swallowed a cow!\n'],
+  ['horse', 'She\'s dead, of course!\n']
 ];
 
 /* Return string for a particular verse */
 FoodChain.prototype.verse = function verse(n) {
   let first = n - 1;
-  let result = [];
+  let result = '';
   /* Start with swallowing first animal and comment */
-  result.push(`I know an old lady who swallowed a ${this.text[first][0]}.`);
-  result.push(this.text[first][1]);
+  result += `I know an old lady who swallowed a ${this.text[first][0]}.\n`;
+  result += this.text[first][1];
   /* If this is the last verse, skip next part */
   if (first < this.text.length - 1) {
     /* Keep swallowing other animals */
     for (let i = first - 1; i >= 0; i--) {
-      result.push(`She swallowed the ${this.text[i + 1][0]} to catch the ${this.text[i][2] || this.text[i][0]}.`);
+      result += `She swallowed the ${this.text[i + 1][0]} to catch the ${this.text[i][2] || this.text[i][0]}.\n`;
     }
     /* Ending statement */
     if (first > 0) {
-      result.push('I don\'t know why she swallowed the fly. Perhaps she\'ll die.');
+      result += 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n';
     }
   }
-  return result.join('\n') + '\n';
+  return result;
 };
 
 /* Return the string for a sequence of verses */
 FoodChain.prototype.verses = function verses(first, last) {
-  let results = [];
+  let results = '';
   for (let i = first; i <= last; i++) {
-    results.push(this.verse(i));
+    results += this.verse(i) + '\n';
   }
-  return results.join('\n') + '\n';
+  return results;
 };
 
 module.exports = FoodChain;
