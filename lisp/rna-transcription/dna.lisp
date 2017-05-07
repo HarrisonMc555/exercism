485b8d7cc4646a33c77c7c86810f326ad6edbd69
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-07 11:44:03 -0500

    Finished lisp/rna-transcription.

---
+++ lisp/rna-transcription/dna.lisp
@@ -0,0 +1,19 @@
+(in-package #:cl-user)
+(defpackage #:dna
+  (:use #:cl)
+  (:export #:convert-base)
+  (:export #:to-rna))
+(in-package #:dna)
+
+(defun convert-base (base)
+  "Transform a DNA nucleotide to an RNA nucleotide"
+  (case base
+    (#\G #\C)
+    (#\C #\G)
+    (#\T #\A)
+    (#\A #\U)
+    (otherwise (throw :dna-error "Invalid DNA nucleotide"))))
+
+(defun to-rna (str)
+  "Transcribe a string representing DNA nucleotides to RNA."
+  (map 'string #'convert-base str))
