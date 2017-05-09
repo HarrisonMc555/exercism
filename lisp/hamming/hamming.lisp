035105c784531cb54e4c8eb4841dc361f2d66701
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-08 22:32:45 -0500

    Used advice to improve lisp/hamming.

--- lisp/hamming/hamming.lisp
+++ lisp/hamming/hamming.lisp
@@ -6,11 +6,5 @@
 
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands."
-  ;; If not the same length, then nil
-  (if (not (= (length dna1) (length dna2)))
-      nil
-      ;; If same length, see how many characters are unequal
-      (length (remove t (mapcar 'eql
-                                ;; Get a list of characters
-                                (map 'list #'identity dna1)
-                                (map 'list #'identity dna2))))))
+  (when (= (length dna1) (length dna2))
+    (count nil (map 'list #'eql dna1 dna2))))
