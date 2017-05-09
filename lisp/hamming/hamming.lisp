2772ebd2184f545bc2058f00e361039536425371
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-09 08:18:03 -0500

    Improved lisp/hamming.

--- lisp/hamming/hamming.lisp
+++ lisp/hamming/hamming.lisp
@@ -7,4 +7,4 @@
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands."
   (when (= (length dna1) (length dna2))
-    (count nil (map 'list #'eql dna1 dna2))))
+    (count nil (map 'list #'char= dna1 dna2))))
