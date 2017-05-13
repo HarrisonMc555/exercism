5a8c9f98a1d3da5a48d77bfd5594d7e3e2e69069
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-13 09:33:11 -0500

    Finished haskell/bob.

---
+++ haskell/bob/src/Bob.hs
@@ -0,0 +1,18 @@
+module Bob (responseFor) where
+
+import Data.Char
+
+responseFor :: String -> String
+responseFor input | isSilence  input = "Fine. Be that way!"
+                  | isYelling  input = "Whoa, chill out!"
+                  | isQuestion input = "Sure."
+                  | otherwise        = "Whatever."
+
+isYelling input = any isAlpha input && input == map toUpper input
+
+isQuestion input = (last $ strip input) == '?'
+
+isSilence input = strip input == ""
+
+strip = f . f
+  where f = reverse . dropWhile isSpace
