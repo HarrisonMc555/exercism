ac151a23544a7872bfbad91d7f1174b87b02fbd2
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-14 21:51:38 -0500

    Cleaned javascript/etl.

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,13 +1,18 @@
 'use strict';
 
-const ETL = function ETL(input) {};
+const ETL = function ETL() {};
 
 ETL.prototype.transform = function transform(oldScores) {
   let result = {};
   for (let score in oldScores) {
-    for (let i = 0; i < oldScores[score].length; i++) {
-      let letter = oldScores[score][i];
-      result[letter.toLowerCase()] = Number.parseInt(score);
+    /* This check is to make eslint happy. I could tell eslint to ignore it, but
+     * this is just for reference right now.
+     */
+    if (ETL.prototype.hasOwnProperty.call(oldScores, score)) {
+      for (let i = 0; i < oldScores[score].length; i++) {
+        let letter = oldScores[score][i];
+        result[letter.toLowerCase()] = Number.parseInt(score, 10);
+      }
     }
   }
   return result;
