815a645e161531267aec31012a2f815926cd5e73
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-14 20:12:42 -0500

    Continued cpp/sieve.
    I think I solved the problem but I don't know how to run the
    tests.

--- cpp/sieve/sieve.cpp
+++ cpp/sieve/sieve.cpp
@@ -5,12 +5,36 @@
 using namespace std;
 
 vector<int> primes(int max) {
+    // cout << "primes(" << max << ")" << endl;
     vector<int> primes = vector<int>();
     bool erat[max];
-    erat[2] = true;
+    for (int i = 0; i < max; i++) { erat[i] = false; }
+    for (int n = 2; n < max; n++) {
+        // cout << "\tn:" << n << endl;
+        if (erat[n]) {
+            // cout << "\t\t--not " << n << endl;
+            continue;
+        }
+        primes.push_back(n);
+        for (int i = 2*n; i < max; i += n) {
+            // cout << "\t\ti:" << i << endl;
+            erat[i] = true;
+        }
+    }
     return primes;
 }
 
+void printPrimes(vector<int> primes) {
+    for (int x : primes) {
+        cout << x << ", ";
+    }
+}
+
 int main() {
-    return 0;
+    vector<int> p10 = primes(10);
+    cout << "primes(10): ";
+    printPrimes(p10);
+    vector<int> p1000 = primes(1000);
+    cout << "primes(1000): ";
+    printPrimes(p1000);
 }
