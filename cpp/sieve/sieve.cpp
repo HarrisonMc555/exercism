74f9578586c8b0cc9c97d1db6cc7a6d01953be0d
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-14 19:45:02 -0500

    Started cpp/sieve.

---
+++ cpp/sieve/sieve.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include "sieve.h"
+
+using namespace std;
+
+vector<int> primes(int max) {
+    vector<int> primes = vector<int>();
+    bool erat[max];
+    erat[2] = true;
+    return primes;
+}
+
+int main() {
+    return 0;
+}
---
+++ cpp/sieve/sieve.h
@@ -0,0 +1,10 @@
+#ifndef SIEVE_H
+#define SIEVE_H
+
+#include <vector>
+
+using namespace std;
+
+vector<int> primes(int max);
+
+#endif
