1a9d95769d8b8dcd2fcb2a21a0056728101a67ce
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-21 12:29:54 -0500

    Finished haskell/grains.

---
+++ haskell/grains/src/Grains.hs
@@ -0,0 +1,11 @@
+module Grains (square, total) where
+
+import Data.Maybe (fromJust)
+
+square :: Integer -> Maybe Integer
+square n = if 0 < n && n <= 64
+           then Just $ 2 ^ (n - 1)
+           else Nothing
+
+total :: Integer
+total = sum $ map fromJust $ map square [1..64]
