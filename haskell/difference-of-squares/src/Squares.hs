0069740a1ea40cd318354916465c8e4059a81b48
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-21 12:01:25 -0500

    Finished haskell/difference-of-squares.

---
+++ haskell/difference-of-squares/src/Squares.hs
@@ -0,0 +1,13 @@
+module Squares (difference, squareOfSums, sumOfSquares) where
+
+difference :: Integral a => a -> a
+difference n = (squareOfSums n) - (sumOfSquares n)
+
+squareOfSums :: Integral a => a -> a
+squareOfSums n = square $ sum [1..n]
+
+sumOfSquares :: Integral a => a -> a
+sumOfSquares n = sum $ map square [1..n]
+
+square :: Integral a => a -> a
+square n = n * n
