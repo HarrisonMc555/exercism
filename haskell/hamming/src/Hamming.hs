ef2b8a08c28697be1bf4f1b19a72dbfcab57fb75
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-23 16:23:12 -0500

    Improved (?) haskell/hamming.

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,8 +1,16 @@
 module Hamming (distance) where
 
+-- distance :: String -> String -> Maybe Int
+-- distance xs ys = if length xs == length ys
+--                  then Just $ count diff $ zip xs ys
+--                  else Nothing
+--   where count f = length . filter f
+--         diff pair = fst pair /= snd pair
+
 distance :: String -> String -> Maybe Int
-distance xs ys = if length xs == length ys
-                 then Just $ count diff $ zip xs ys
-                 else Nothing
-  where count f = length . filter f
-        diff pair = fst pair /= snd pair
+distance [] []         = Just 0
+distance (x:xs) (y:ys) = xydiff `plus` distance xs ys
+  where xydiff = if x == y then Just 0 else Just 1
+        plus (Just a) (Just b) = Just $ a + b
+        plus _ _               = Nothing
+distance _ _           = Nothing
