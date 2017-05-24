8ec6b74cae88e1d0a9890fcc75c235e35d08a02a
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-23 21:08:30 -0500

    Cleaned haskell/hamming.
    Used Applicitave nature of Maybe.

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,16 +1,8 @@
 module Hamming (distance) where
 
--- distance :: String -> String -> Maybe Int
--- distance xs ys = if length xs == length ys
---                  then Just $ count diff $ zip xs ys
---                  else Nothing
---   where count f = length . filter f
---         diff pair = fst pair /= snd pair
-
 distance :: String -> String -> Maybe Int
-distance [] []         = Just 0
-distance (x:xs) (y:ys) = xydiff `plus` distance xs ys
-  where xydiff = if x == y then Just 0 else Just 1
-        plus (Just a) (Just b) = Just $ a + b
-        plus _ _               = Nothing
-distance _ _           = Nothing
+distance [] [] = Just 0
+distance (x:xs) (y:ys)
+  | x == y    =  distance xs ys
+  | otherwise = (1+) <$> distance xs ys
+distance _ _ = Nothing
