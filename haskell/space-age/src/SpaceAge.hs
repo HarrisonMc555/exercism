7d1ef2d1710fadda510f15e6e371127a42486bf1
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-29 09:26:23 -0500

    Cleaned haskell/space-age.

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -9,15 +9,16 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
-planetYearsPerEarthYear Mercury = 0.2408467
-planetYearsPerEarthYear Venus   = 0.61519726
-planetYearsPerEarthYear Earth   = 1.0
-planetYearsPerEarthYear Mars    = 1.8808158
-planetYearsPerEarthYear Jupiter = 11.862615
-planetYearsPerEarthYear Saturn  = 29.447498
-planetYearsPerEarthYear Uranus  = 84.016846
-planetYearsPerEarthYear Neptune = 164.79132
-planetYearsPerEarthYear other   = error "Input is not a planet."
+planetYearsPerEarthYear planet = case planet of
+  Mercury -> 0.2408467
+  Venus   -> 0.61519726
+  Earth   -> 1.0
+  Mars    -> 1.8808158
+  Jupiter -> 11.862615
+  Saturn  -> 29.447498
+  Uranus  -> 84.016846
+  Neptune -> 164.79132
+  _       -> error "Input is not a planet."
 
 earthSecondsPerYear = 31557600
 
