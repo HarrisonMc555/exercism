2f148588eec608b189e81b7731bff8e7fea9274f
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-29 08:04:21 -0500

    Cleaned python/rna-transcription.

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,37 +1,17 @@
-"""
-Exercism.io RNA Kata
-"""
-
-CONVERSION = [('G', 'C'),
-              ('C', 'G'),
-              ('T', 'A'),
-              ('A', 'U')]
+CONVERSION = {'G': 'C',
+              'C': 'G',
+              'T': 'A',
+              'A': 'U'}
 
 
-def convert_nucleotide(nucleotide):
-    """Converts DNA nucleotide to RNA"""
-    for complement in CONVERSION:
-        if complement[0] == nucleotide:
-            return complement[1]
-
-
-def validate_dna_sequence(dna_sequence):
-    """Validates a DNA sequence"""
-    is_valid = True
-
-    if not dna_sequence:
-        return False
-
-    for nucleotide in dna_sequence:
-        if nucleotide not in dict(CONVERSION):
-            return False
-
-    return is_valid
+def convert_base(b):
+    """Returns RNA base complement for DNA base."""
+    return CONVERSION[b]
 
 
 def to_rna(dna_sequence):
-    """Returns RNA complement of DNA sequence"""
-    if validate_dna_sequence(dna_sequence):
-        return ''.join(map(convert_nucleotide, list(dna_sequence)))
-    else:
+    """Returns RNA complement of DNA sequence."""
+    try:
+        return ''.join(map(convert_base, dna_sequence))
+    except KeyError:
         return ''
