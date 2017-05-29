b351a2f747e12c2051a3027cf65cf4caedce0a23
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-29 07:46:09 -0500

    Cleaned python/hamming.

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,22 +1,11 @@
-"""
-Exercism.io Hamming kata
-"""
-
-
-def validate_dna_strands(strand_a, strand_b):
-    """Raises ValueError if strands are not valid"""
-    if len(strand_a) is not len(strand_b):
-        raise ValueError("Strands must be of the same length")
+def diff(pair):
+    """Returns 0 when pair elements are equal, otherwise 1"""
+    x, y = pair
+    return 0 if x == y else 1
 
 
 def distance(strand_a, strand_b):
     """Returns hamming distance of two dna strands"""
-    validate_dna_strands(strand_a, strand_b)
-
-    count = 0
-
-    for index, nucleotide in enumerate(strand_a):
-        if nucleotide is not strand_b[index]:
-            count += 1
-
-    return count
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of the same length")
+    return sum(map(diff, zip(strand_a, strand_b)))
