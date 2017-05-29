6073f43e65a45e62733384b67f85a8defd239eb3
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-29 07:56:31 -0500

    Cleaned python/pangram.

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,20 +1,7 @@
-"""
-Exercism.io pangram kata
-"""
-
 from string import ascii_lowercase
-
-
-def valid_letter(letter):
-    """Checks if letter is in the ascii alphabet"""
-    return letter in ascii_lowercase
-
-
-def get_unique_letters(sentence):
-    """Returns a set of letters used in a sentence"""
-    return set(filter(valid_letter, list(sentence.lower())))
 
 
 def is_pangram(sentence):
     """Determines if a sentence is a pangram"""
-    return 26 is len(get_unique_letters(sentence))
+    s = sentence.lower()
+    return all([c in s for c in ascii_lowercase])
