c058d47a204cef80d1bc13d5ef8ee15830342c17
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-29 08:35:56 -0500

    Cleaned cpp/sieve.

--- cpp/sieve/sieve.cpp
+++ cpp/sieve/sieve.cpp
@@ -3,20 +3,19 @@
 #include "sieve.h"
 
 namespace sieve {
+    /* Returns all primes below max using Eratosthenes sieve */
     std::vector<int> primes(int max) {
-        // cout << "primes(" << max << ")" << endl;
         std::vector<int> primes = std::vector<int>();
         bool erat[max];
-        for (int i = 0; i < max; i++) { erat[i] = false; }
+        for (int i = 0; i < max; i++) {
+            erat[i] = false;
+        }
         for (int n = 2; n < max; n++) {
-            // cout << "\tn:" << n << endl;
             if (erat[n]) {
-                // cout << "\t\t--not " << n << endl;
                 continue;
             }
             primes.push_back(n);
             for (int i = 2*n; i < max; i += n) {
-                // cout << "\t\ti:" << i << endl;
                 erat[i] = true;
             }
         }
