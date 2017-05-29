b3dc077c734986bd55c95e656e1fd370c021fa68
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-29 07:50:46 -0500

    Cleaned python/leap.

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,5 +1,13 @@
+def divisible(num, div):
+    """Return True if num is divisible by div"""
+    return num % div == 0
+
+
 def is_leap_year(year):
-    if year % 25:
-        return year % 4 == 0
-    else:
-        return year % 16 == 0
+    """Returns True if year is a leap year
+
+    A year is a leap year if it is divisible by 4, unless it is divisible by
+    100, unless it is also divisible by 400.
+    """
+    return divisible(year, 4) and not divisible(year, 100) or divisible(year,
+                                                                        400)
