3cf5b8cefaf60c39f24b28e525cc2ad0b93332ad
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-30 19:08:46 +0100

    Finished haskell/raindrops.

---
+++ haskell/raindrops/src/Raindrops.hs
@@ -0,0 +1,18 @@
+module Raindrops (convert) where
+
+convert :: Int -> String
+convert n
+  | rains == "" = show n
+  | otherwise   = rains
+  where rains = getRains n
+
+rainList :: [(Int, String)]
+rainList = [ (3, "Pling")
+           , (5, "Plang")
+           , (7, "Plong")]
+
+getRains :: Int -> String
+getRains n = concatMap snd $ filter (divisibleBy n . fst) rainList
+
+divisibleBy :: Integral a => a -> a -> Bool
+divisibleBy num divisor = num `mod` divisor == 0
