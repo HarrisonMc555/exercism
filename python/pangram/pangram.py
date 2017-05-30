f6b231466e221711eeb9e5179dcea9f4c4cc4d35
Harrison McCullough <mccullough.harrison@gmail.com>
2017-05-29 22:37:20 -0500

    Cleaned python/pangram.

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -4,4 +4,4 @@ from string import ascii_lowercase
 def is_pangram(sentence):
     """Determines if a sentence is a pangram."""
     s = sentence.lower()
-    return all([c in s for c in ascii_lowercase])
+    return all(c in s for c in ascii_lowercase)
