26d363b0a8a6aa3c531b00ed4e567ac000e7b815
Harrison McCullough <mccullough.harrison@gmail.com>
2017-06-10 08:33:37 -0500

    Cleaned haskell/difference-of-squares.

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -4,10 +4,17 @@ difference :: Integral a => a -> a
 difference n = squareOfSums n - sumOfSquares n
 
 squareOfSums :: Integral a => a -> a
-squareOfSums n = square $ sum [1..n]
+squareOfSums n = let x = (n*(n+1) `div` 2) in
+  x*x
 
 sumOfSquares :: Integral a => a -> a
-sumOfSquares n = sum $ map square [1..n]
+sumOfSquares n = n*(n+1)*(2*n + 1) `div` 6
 
-square :: Integral a => a -> a
-square x = x ^ (2 :: Integer)
+_squareOfSums :: Integral a => a -> a
+_squareOfSums n = _square $ sum [1..n]
+
+_sumOfSquares :: Integral a => a -> a
+_sumOfSquares n = sum $ map _square [1..n]
+
+_square :: Integral a => a -> a
+_square x = x ^ (2 :: Integer)
