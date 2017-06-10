b7506090a488cf02868cc65c2f817cbd18e00140
Harrison McCullough <mccullough.harrison@gmail.com>
2017-06-10 08:48:14 -0500

    Cleaned haskelll/strain.

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -4,17 +4,16 @@ difference :: Integral a => a -> a
 difference n = squareOfSums n - sumOfSquares n
 
 squareOfSums :: Integral a => a -> a
-squareOfSums n = let x = (n*(n+1) `div` 2) in
-  x*x
+squareOfSums n = square (n*(n+1) `div` 2)
 
 sumOfSquares :: Integral a => a -> a
 sumOfSquares n = n*(n+1)*(2*n + 1) `div` 6
 
 _squareOfSums :: Integral a => a -> a
-_squareOfSums n = _square $ sum [1..n]
+_squareOfSums n = square $ sum [1..n]
 
 _sumOfSquares :: Integral a => a -> a
-_sumOfSquares n = sum $ map _square [1..n]
+_sumOfSquares n = sum $ map square [1..n]
 
-_square :: Integral a => a -> a
-_square x = x ^ (2 :: Integer)
+square :: Integral a => a -> a
+square x = x ^ (2 :: Integer)
--- haskell/strain/src/Strain.hs
+++ haskell/strain/src/Strain.hs
@@ -1,7 +1,11 @@
 module Strain (keep, discard) where
 
 discard :: (a -> Bool) -> [a] -> [a]
-discard p = filter (not . p)
+discard p = keep (not . p)
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep = filter
+keep _ [] = []
+keep f (x:xs)
+  | f x       = x : rest
+  | otherwise = rest
+  where rest = keep f xs
