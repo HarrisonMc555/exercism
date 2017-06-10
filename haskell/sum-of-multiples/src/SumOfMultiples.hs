7ae9d57d83b14ebd660372c895256d790a1d3e26
Harrison McCullough <mccullough.harrison@gmail.com>
2017-06-10 09:10:35 -0500

    Cleaned haskell/sum-of-multiples.

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -10,12 +10,11 @@ listOfMultiples factors limit = unique allMultiples
   where allMultiples = concatMap (`multiples` limit) factors
 
 multiples :: Integer -> Integer -> [Integer]
-multiples factor limit = takeWhile withinLimit $ stride intFactor [factor..]
-  where withinLimit x = x < limit
-        intFactor = fromIntegral factor
-        stride s = go
-          where go (x:xs) = x : go (drop (s - 1) xs)
-                go _      = []
+multiples factor limit = takeWhile (< limit) $ stride intFactor [factor..]
+  where intFactor = fromIntegral factor
+        stride _ []     = []
+        stride s (x:xs) = x : stride s rest
+          where rest = drop (s-1) xs
 
 unique :: (Ord a) => [a] -> [a]
 unique = map head . group . sort
