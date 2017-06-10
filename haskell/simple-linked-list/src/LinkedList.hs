ddd7b759feaf149c77afbb8010df092f39a0ae74
Harrison McCullough <mccullough.harrison@gmail.com>
2017-06-10 10:14:07 -0500

    Finished haskell/simple-linked-list.

---
+++ haskell/simple-linked-list/src/LinkedList.hs
@@ -0,0 +1,48 @@
+module LinkedList
+    ( LinkedList
+    , datum
+    , fromList
+    , isNil
+    , new
+    , next
+    , nil
+    , reverseLinkedList
+    , toList
+    ) where
+
+data LinkedList a = Node a (LinkedList a)
+                  | Nil
+                  deriving (Eq, Show)
+
+instance Foldable LinkedList where
+  foldMap f (Node a rest) = f a `mappend` foldMap f rest
+  foldMap _ Nil           = mempty
+
+datum :: LinkedList a -> a
+datum (Node a _) = a
+datum Nil        = error "empty linked list"
+
+fromList :: [a] -> LinkedList a
+fromList = foldr new nil
+
+isNil :: LinkedList a -> Bool
+isNil (Node _ _) = False
+isNil Nil        = True
+
+new :: a -> LinkedList a -> LinkedList a
+new = Node
+
+next :: LinkedList a -> LinkedList a
+next (Node _ rest) = rest
+next Nil           = error "empty linked list"
+
+nil :: LinkedList a
+nil = Nil
+
+reverseLinkedList :: LinkedList a -> LinkedList a
+reverseLinkedList = foldl append Nil
+  where append l a = new a l
+
+toList :: LinkedList a -> [a]
+toList (Node a rest) = a : toList rest
+toList Nil           = []
