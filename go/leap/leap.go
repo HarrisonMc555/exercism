14bb9c7da4ba27e3ae480563f2c86fb2efe960bf
Harrison McCullough <mccullough.harrison@gmail.com>
2017-06-21 20:59:49 +0100

    Gofmt go/leap

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -5,8 +5,12 @@ const testVersion = 3
 
 // IsLeapYear returns true if year is a leap year
 func IsLeapYear(year int) bool {
-	isDivisble := func(x, y int) bool {
-		return x % y == 0
+	multiple := func(x int) bool {
+		return divisibleBy(year, x)
 	}
-	return isDivisble(year, 4) && (!isDivisble(year, 100) || isDivisble(year, 400))
+	return multiple(4) && (!multiple(100) || multiple(400))
+}
+
+func divisibleBy(x, y int) bool {
+	return x%y == 0
 }
