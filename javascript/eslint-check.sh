85027aa8762010693978e403c609019ff1ec3f6b
Harrison McCullough <mccullough.harrison@gmail.com>
2017-06-26 22:20:02 -0500

    Improve eslint-check.sh for javascript

--- javascript/eslint-check.sh
+++ javascript/eslint-check.sh
@@ -1,1 +1,10 @@
-eslint $(find /home/harrison/exercism/javascript/ -mindepth 2 -name '*.js' -not -name '*.spec.js')
+#!/bin/bash
+
+cd "/home/harrison/exercism/javascript/"
+for f in $(find . \
+                -mindepth 2 \
+                -name '*.js' \
+                -not -name '*.spec.js' \
+                -not -path '*node_modules*'); do
+    eslint "$f" && echo "no lint in $f";
+done
