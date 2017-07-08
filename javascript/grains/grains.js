5fa692efbd7dd8d0c7e61ee2654642b9c5cab8a0
Harrison McCullough <mccullough.harrison@gmail.com>
2017-07-08 18:17:13 +0100

    Clean javascript/grains

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -1,16 +1,15 @@
 const bigInt = require('big-integer');
 
+const totalGrains = bigInt(2).pow(64).minus(1).toString();
+
 const Grains = function Grains() {
   this.square = function square(n) {
     return bigInt(2).pow(n - 1).toString();
   };
 
-  this.total = (function createTotalFunc() {
-    const totalGrains = bigInt(2).pow(64).minus(1).toString();
-    return function total() {
-      return totalGrains;
-    };
-  }());
+  this.total = function total() {
+    return totalGrains;
+  };
 };
 
 module.exports = Grains;
