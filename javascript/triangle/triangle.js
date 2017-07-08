5bc530b4cf31119d03b9816a6d22de5c39f7135f
Harrison McCullough <mccullough.harrison@gmail.com>
2017-07-08 18:17:18 +0100

    Clean javascript/triangle

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,27 +1,17 @@
-const Triangle = function Triangle(a, b, c) {
-  this.kind = (function kindFunc() {
-    /* https://stackoverflow.com/a/1063027/7343786 */
-    function sortNumber(x, y) {
-      return x - y;
+function Triangle(a, b, c) {
+  /* https://stackoverflow.com/a/1063027/7343786 */
+  const [x, y, z] = [a, b, c].sort((i, j) => i - j);
+
+  this.kind = function kind() {
+    if (x <= 0 || x + y <= z) {
+      throw Error('Invalid triangle');
+    } else if (x === y && y === z) {
+      return 'equilateral';
+    } else if (x === y || y === z) {
+      return 'isosceles';
     }
-
-    const sides = [a, b, c];
-    sides.sort(sortNumber);
-    const x = sides[0];
-    const y = sides[1];
-    const z = sides[2];
-
-    return function kind() {
-      if (x <= 0 || x + y <= z) {
-        throw Error('Invalid triangle');
-      } else if (x === y && y === z) {
-        return 'equilateral';
-      } else if (x === y || y === z) {
-        return 'isosceles';
-      }
-      return 'scalene';
-    };
-  }());
+    return 'scalene';
+  };
 };
 
 module.exports = Triangle;
