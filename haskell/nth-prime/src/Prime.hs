86439ce6a4b9b39e8f821f51766511e4bc656c01
Harrison McCullough <harrison.mccullough@labs.att.com>
2017-07-11 14:54:58 -0500

    Finish haskell\nth-prime

---
+++ haskell/nth-prime/src/Prime.hs
@@ -0,0 +1,12 @@
+module Prime (nth) where
+
+nth :: Int -> Maybe Integer
+nth n
+  | n <= 0 = Nothing
+  | otherwise = Just $ primes !! (n-1)
+
+-- Inspired by haskell.org
+primes :: [Integer]
+primes = filterPrimes [2..]
+  where filterPrimes (p:xs) =
+          p : filterPrimes [x | x <- xs, x `mod` p /= 0]
