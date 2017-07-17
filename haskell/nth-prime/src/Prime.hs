78624da01f5737d51cfd42416b86a6f68375481e
Harrison McCullough <mccullough.harrison@gmail.com>
2017-07-17 08:04:10 -0500

    Fix haskell/nth-prime
    Using the primes package to significantly speed things up.

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,12 +1,14 @@
 module Prime (nth) where
 
+import Data.Numbers.Primes (primes)
+
 nth :: Int -> Maybe Integer
 nth n
   | n <= 0 = Nothing
   | otherwise = Just $ primes !! (n-1)
 
--- Inspired by haskell.org
-primes :: [Integer]
-primes = filterPrimes [2..]
-  where filterPrimes (p:xs) =
-          p : filterPrimes [x | x <- xs, x `mod` p /= 0]
+-- -- Inspired by haskell.org
+-- primes :: [Integer]
+-- primes = filterPrimes [2..]
+--   where filterPrimes (p:xs) =
+--           p : filterPrimes [x | x <- xs, x `mod` p /= 0]
