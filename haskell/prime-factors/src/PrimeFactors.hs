f3f1046a4a6805ad37085810856240200f23300a
Harrison McCullough <mccullough.harrison@gmail.com>
2017-07-17 08:02:16 -0500

    Fix haskell/prime-factors
    My solution was too slow to complete the final test case, so I'm using
    the primes package instead, which takes 0.34 seconds on the largest
    test case.

--- haskell/prime-factors/src/PrimeFactors.hs
+++ haskell/prime-factors/src/PrimeFactors.hs
@@ -3,7 +3,7 @@ module PrimeFactors (primeFactors) where
 
 import Data.List (find)
 import Data.Maybe (fromJust)
--- import Data.Numbers.Primes (primes)
+import Data.Numbers.Primes (primes)
 
 primeFactors :: Integer -> [Integer]
 primeFactors n = getResult . find done . scanl addFactors (n, []) $ primes
@@ -11,15 +11,19 @@ primeFactors n = getResult . find done . scanl addFactors (n, []) $ primes
         getResult = reverse . snd . fromJust
 
 addFactors :: (Integer, [Integer]) -> Integer -> (Integer, [Integer])
-addFactors (n, fs) p
-  | n == 1    = (n, fs)
+addFactors nfs@(n, fs) p
+  | n == 1    = nfs
   | composite = addFactors (q, p:fs) p
-  | otherwise = (n, fs)
+  | otherwise = nfs
   where (q, r)    = n `quotRem` p
         composite = r == 0
 
--- Inspired by haskell.org
-primes :: [Integer]
-primes = filterPrimes [2..]
-  where filterPrimes (p:xs) =
-          p : filterPrimes [x | x <- xs, x `mod` p /= 0]
+-- If I use this, the final test (93819012551) takes far too long. I haven't
+-- timed it yet, because I'm not sure how long it would take. I waited for 5
+-- minutes and it hadn't stopped yet. It only takes 0.34 seconds with the primes
+-- package though.
+-- -- Inspired by haskell.org
+-- primes :: [Integer]
+-- primes = filterPrimes [2..]
+--   where filterPrimes (p:xs) =
+--           p : filterPrimes [x | x <- xs, x `mod` p /= 0]
