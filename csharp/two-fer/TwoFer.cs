56978b0badbb701c26f63d8631791caa914ea6a2
Harrison McCullough <mccullough.harrison@gmail.com>
2017-08-23 20:09:40 +0100

    Finish csharp/two-fer

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class TwoFer
+{
+    public static string Name(string input = null)
+    {
+        string name = input ?? "you";
+        return "One for " + name + ", one for me.";
+    }
+}
