91ed21c95e592a2ef996bc9066b9ec0be1e55bb6
Harrison McCullough <mccullough.harrison@gmail.com>
2017-09-11 22:20:14 -0600

    Finish haskell/linked-list

---
+++ haskell/linked-list/src/Deque.hs
@@ -0,0 +1,98 @@
+module Deque (Deque, mkDeque, pop, push, shift, unshift) where
+
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+
+
+data Node a = Node { prev  :: NodeRef a
+                   , value :: a
+                   , next  :: NodeRef a
+                   }
+            | Nil
+type NodeRef a = IORef (Node a)
+data Deque a = Deque { head :: IORef (NodeRef a)
+                     , tail :: IORef (NodeRef a)
+                     }
+
+mkDeque :: IO (Deque a)
+mkDeque = do
+  h <- mkEmptyNodeRef
+  t <- mkEmptyNodeRef
+  href <- newIORef h
+  tref <- newIORef t
+  return (Deque href tref)
+
+mkEmptyNodeRef :: IO (NodeRef a)
+mkEmptyNodeRef = newIORef Nil
+
+mkNodeRef :: a -> IO (NodeRef a)
+mkNodeRef a = do
+  p <- mkEmptyNodeRef
+  n <- mkEmptyNodeRef
+  newIORef (Node p a n)
+
+pop :: Deque a -> IO (Maybe a)
+pop (Deque href tref) = do
+  h <- readIORef href
+  hval <- readIORef h
+  case hval of
+    (Node pref hv nref) -> do
+      writeIORef href nref
+      n <- readIORef nref
+      case n of
+        (Node _ nv nnref) -> writeIORef nref (Node pref nv nnref)
+        Nil -> writeIORef tref nref
+      return (Just hv)
+    Nil -> return Nothing
+
+push :: Deque a -> a -> IO ()
+push (Deque href tref) v = do
+  newNode <- mkNodeRef v
+  (Node newP _ newN) <- readIORef newNode
+  h <- readIORef href
+  hval <- readIORef h
+  writeIORef href newNode
+  case hval of
+    Nil ->
+      writeIORef tref newNode
+    Node _ headV headN ->
+      writeIORef h (Node newNode headV headN) >>
+      writeIORef newNode (Node newP v h)
+
+unshift :: Deque a -> a -> IO ()
+unshift (Deque href tref) v = do
+  newNode <- mkNodeRef v
+  (Node newP _ newN) <- readIORef newNode
+  t <- readIORef tref
+  tval <- readIORef t
+  writeIORef tref newNode
+  case tval of
+    Nil ->
+      writeIORef href newNode
+    Node tailP tailV _ ->
+      writeIORef t (Node tailP tailV newNode) >>
+      writeIORef newNode (Node t v newN)
+
+shift :: Deque a -> IO (Maybe a)
+shift (Deque href tref) = do
+  t <- readIORef tref
+  tval <- readIORef t
+  case tval of
+    (Node pref tv nref) -> do
+      writeIORef tref pref
+      p <- readIORef pref
+      case p of
+        (Node ppref pv _) -> writeIORef pref (Node ppref pv nref)
+        Nil -> writeIORef href pref
+      return (Just tv)
+    Nil -> return Nothing
+deleteNode :: Node a -> IO ()
+deleteNode Nil = return ()
+deleteNode (Node pRef v nRef) = do
+  p <- readIORef pRef
+  case p of
+    Node ppRef pv _ -> writeIORef pRef (Node ppRef pv nRef)
+    Nil -> return ()
+  n <- readIORef nRef
+  case n of
+    Node _ nv nnRef -> writeIORef nRef (Node pRef nv nnRef)
+    Nil -> return ()
