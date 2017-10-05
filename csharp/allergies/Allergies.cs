7fb97426447ea3d5979bea12e9abe2013d7a7973
Harrison McCullough <mccullough.harrison@gmail.com>
2017-10-05 15:13:17 -0600

    Finish csharp/allergies

---
+++ csharp/allergies/Allergies.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Allergies
+{
+    private static readonly Dictionary<string, int> AllergyDict = new Dictionary<String, int>()
+    {
+        { "eggs",         1 },
+        { "peanuts",      2 },
+        { "shellfish",    4 },
+        { "strawberries", 8 },
+        { "tomatoes",     16 },
+        { "chocolate",    32 },
+        { "pollen",       64 },
+        { "cats",         128 },
+    };
+
+    private IEnumerable<string> allergies;
+    private int mask;
+
+    public Allergies(int mask)
+    {
+        this.mask = mask;
+        allergies = from kvp in AllergyDict
+                    where IsAllergicTo(kvp.Key, mask)
+                    select kvp.Key;
+    }
+
+    public bool IsAllergicTo(string allergy)
+    {
+        return IsAllergicTo(allergy, mask);
+    }
+
+    public IList<string> List()
+    {
+        return allergies.ToList();
+    }
+
+    private static bool IsAllergicTo(string allergy, int mask)
+    {
+        int allergyScore = AllergyDict[allergy];
+        int scoreTest = mask / allergyScore & 1;
+        return scoreTest == 1;
+    }
+}
