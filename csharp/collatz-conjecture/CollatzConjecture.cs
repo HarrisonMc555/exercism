a83d542ac9ca0ad8e8d89235491eed68f33275b3
Harrison McCullough <mccullough.harrison@gmail.com>
2017-10-09 21:54:48 -0600

    Finish csharp/collatz-conjecture

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    private const int FINAL_COLLATZ_NUMBER = 1;
+
+    public static int Steps(int number)
+    {
+        int steps = 0;
+        while (number != FINAL_COLLATZ_NUMBER)
+        {
+            number = NextCollatzNumber(number);
+            steps++;
+        }
+        return steps;
+    }
+
+    private static int NextCollatzNumber(int n)
+    {
+        if (n <= 0)
+        {
+            throw new ArgumentException("Only positive integers in Collatz sequence");
+        } else if (n == FINAL_COLLATZ_NUMBER)
+        {
+            throw new ArgumentException(FINAL_COLLATZ_NUMBER + " is ending number for Collatz sequence.");
+        } else if (n.IsEven())
+        {
+            return n / 2;
+        } else
+        {
+            return 3 * n + 1;
+        }
+    }
+
+    public static bool IsEven(this int x)
+    {
+        return x % 2 == 0;
+    }
+}
