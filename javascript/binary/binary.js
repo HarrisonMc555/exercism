557da7e12f6599e62baad094540cf433bfde4fd9
Harrison McCullough <mccullough.harrison@gmail.com>
2017-10-12 20:22:21 -0600

    Finish javascript/binary

---
+++ javascript/binary/binary.js
@@ -0,0 +1,23 @@
+function binaryStringToDecimal(binaryString) {
+  const binaryStringPattern = /^[01]+$/;
+  if (!binaryStringPattern.test(binaryString)) {
+    return 0;
+  }
+  const base = 2;
+  let totalNumber = 0;
+  let currentPower = 1;
+  for (let i = binaryString.length - 1; i >= 0; i -= 1) {
+    totalNumber += parseInt(binaryString[i]) * currentPower;
+    currentPower *= base;
+  }
+  return totalNumber;
+}
+
+const Binary = function Binary(binaryString) {
+  this.binaryString = binaryString;
+  this.toDecimal = function toDecimal() {
+    return binaryStringToDecimal(this.binaryString);
+  };
+};
+
+module.exports = Binary;
