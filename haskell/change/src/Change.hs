411275c87cfb5e1f7b3f21d2ea705fb961e1f312
Jacob P. Davis <jacobpdavis7@gmail.com>
2017-10-21 21:59:27 -0600

    Mostly finish haskell/change

--- haskell/change/src/Change.hs
+++ haskell/change/src/Change.hs
@@ -1,56 +1,42 @@
 module Change (findFewestCoins) where
 
-import Data.List (sort, genericReplicate, genericIndex, minimumBy)
-import Data.Maybe (catMaybes, isJust, fromJust)
+import Data.List (sort, genericReplicate, genericIndex, minimumBy, sortOn,
+                  genericIndex, genericDrop)
+import Data.Maybe (catMaybes, isJust, fromJust, listToMaybe)
+import Control.Monad (join)
 
 type Solution = Maybe [Integer]
 
 findFewestCoins :: Integer -> [Integer] -> Solution
-findFewestCoins change coins
-  | change < 0 = Nothing -- no negative change
-  | null coins = Nothing
-  | change < smallestCoinValue = Nothing
-  | otherwise  = Just $ findChange change coins
-  where smallestCoinValue = minimum coins
+findFewestCoins target coins = last solutionsThroughTarget
+  where solutionsThroughTarget =
+          scanIterate nextSolution' base `genericIndex` target
+        nextSolution' = nextSolution coins
+        base = Just []
+
+nextSolution :: [Integer] -> [Solution] -> Solution
+nextSolution coins prevSolutions =
+  listToMaybe $ sortOn length $ catMaybes $ map coinSoln coins
+  where coinSoln coin = (coin:) <$> (join $ maybeIndex prevSolutions (coin - 1))
+
+maybeIndex :: Integral i => [a] -> i -> Maybe a
+maybeIndex as i = listToMaybe $ genericDrop i as
+
+scanIterate :: ([a] -> a) -> a -> [[a]]
+scanIterate f base = map reverse $ iterate g [base]
+  where g xs = f xs:xs
 
 
-findChange :: Integer -> [Integer] -> [Integer]
-findChange change coins =
-  let coins' = sort coins
-      useCoin coin (changeLeft, coinsSoFar) =
-        let (numCoins, changeLeft') = changeLeft `quotRem` coin
-            coinsToAdd = genericReplicate numCoins coin
-        in (changeLeft', coinsToAdd ++ coinsSoFar)
-      (changeLeftOver, changeCoins) = foldr useCoin (change, []) coins'
-  in changeCoins
+----------------------------------------
+-- Unused but cool code
 
--- findFewestCoins' :: Integer -> [Integer] -> Solution
--- findFewestCoins' change coins =
---   let prevSolution = findFewestCoins' (change - 1) coins
---   in if change < 0
---      then Nothing
---      else dynamicFindFewestCoins
+dynamicFoldl :: ([b] -> a -> b) -> b -> [a] -> b
+dynamicFoldl = go
+  where go f b as = head $ dynamicScanl f b as
 
-dynamicFindFewestCoins :: Integer -> [Integer] -> [Solution] -> Solution
-dynamicFindFewestCoins change coins prevSolutions =
-  if invalid || null coinSolutionPairs
-  then Nothing
-  else Just solution
-  where invalid = null coins ||
-                  change < minimum coins
-        coinSolutionPairs = [ (coin, fromJust coinSolution)
-                            | coin <- coins
-                            , let index = change - coin
-                            , index >= 0
-                            , let coinSolution = genericIndex prevSolutions index
-                            , isJust coinSolution
-                            ]
-        orderCoinSolutionPair (_, s1) (_, s2) = length s1 `compare` length s2
-        bestCoinSolutionPair = minimumBy orderCoinSolutionPair coinSolutionPairs
-        solution = case bestCoinSolutionPair
-                   of (coin, solution) -> (coin:) solution
-
--- It would be super cool to have a function like scanl that took the entire
--- lsit of results so far as one of the inputs to the function instead of just
--- the current result so far. You could use that for the actual findFewestCoins
--- function.
+dynamicScanl :: ([b] -> a -> b) -> b -> [a] -> [b]
+dynamicScanl f b = go [b]
+  -- where go :: [b] -> [a] -> [b]
+  where go _ [] = []
+        go bs (a:as) = let b' = f bs a
+                       in b' : go (b':bs) as
