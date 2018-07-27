57d7b84b6934e63c5a35ec453a4e976140b4356c
Harrison McCullough <mccullough.harrison@gmail.com>
2018-07-27 10:46:58 -0600

    Finish haskell/transpose

---
+++ haskell/transpose/src/Transpose.hs
@@ -0,0 +1,31 @@
+module Transpose (transpose) where
+
+import Data.List (tails)
+
+transpose :: [String] -> [String]
+transpose = transposeDefault ' '
+
+transposeDefault :: a -> [[a]] -> [[a]]
+transposeDefault def = transpose' . padListsDescending def
+
+transpose' :: [[a]] -> [[a]]
+transpose' [] = []
+transpose' ([] : xss) = transpose' xss
+transpose' ((x:xs) : xss) =
+  let firstRow = x : [h | (h:_) <- xss]
+      nextRows = transpose' (xs : [t | (_:t) <- xss])
+  in firstRow : nextRows
+
+padRight :: a -> Int -> [a] -> [a]
+padRight x n xs = take n $ xs ++ repeat x
+
+padLists :: a -> [[a]] -> [[a]]
+padLists x xss = let n = maximum [length xs | xs <- xss]
+                 in [padRight x n xs | xs <- xss]
+
+padListsDescending :: a -> [[a]] -> [[a]]
+padListsDescending _ [] = []
+padListsDescending x xss =
+  let lengths = [length xs | xs <- xss]
+      pad xs ls = padRight x (maximum ls) xs
+  in map (uncurry pad) $ zip xss (tails lengths)
