dbe6523385104a72ddc392152aeb5ee7d7bf3045
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-02 12:36:53 -0600

    Finish ruby/two_fer

---
+++ ruby/two-fer/two_fer_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require_relative 'two_fer'
+
+# Common test data version: 1.1.0 c080bdf
+class TwoFerTest < Minitest::Test
+  def test_no_name_given
+    # skip
+    assert_equal "One for you, one for me.", TwoFer.two_fer
+  end
+
+  def test_a_name_given
+    assert_equal "One for Alice, one for me.", TwoFer.two_fer("Alice")
+  end
+
+  def test_another_name_given
+    assert_equal "One for Bob, one for me.", TwoFer.two_fer("Bob")
+  end
+end
