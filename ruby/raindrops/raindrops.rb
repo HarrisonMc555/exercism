36ff3db669e76876a24c416927b6de2cf6d484ce
Harrison McCullough <mccullough.harrison@byu.edu>
2018-09-03 15:13:29 -0600

    Finish ruby/raindrops

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,14 @@
+class Raindrops
+  def self.convert(num)
+    s = ''
+    s += 'Pling' if divisible?(num, 3)
+    s += 'Plang' if divisible?(num, 5)
+    s += 'Plong' if divisible?(num, 7)
+    s = num.to_s if s.empty?
+    s
+  end
+  
+  def self.divisible?(num, div)
+    (num % div).zero?
+  end
+end
