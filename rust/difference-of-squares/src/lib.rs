8374feec12bf44538db1da526ff29a4fd1e7dca9
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-07 09:48:15 -0600

    Finish rust/difference-of-squares

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,19 @@
+fn square(n: u32) -> u32 {
+    n * n
+}
+
+pub fn square_of_sum(n: u32) -> u32 {
+    let sum = (1..n+1).sum();
+    square(sum)
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    (1..n+1)
+        .into_iter()
+        .map(|x| square(x))
+        .sum()
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}
