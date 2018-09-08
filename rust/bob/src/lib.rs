a15323c5b113ccc52efb1d20bfb0ffced6db7443
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-07 22:28:33 -0600

    Finish rust/bob

---
+++ rust/bob/src/lib.rs
@@ -0,0 +1,54 @@
+pub fn reply(message: &str) -> &str {
+    match message_type(message) {
+        MessageType::Question => "Sure.",
+        MessageType::Yelling => "Whoa, chill out!",
+        MessageType::YellingQuestion => "Calm down, I know what I'm doing!",
+        MessageType::Nothing => "Fine. Be that way!",
+        MessageType::Other => "Whatever.",
+    }
+}
+
+fn message_type(message: &str) -> MessageType {
+    let yelling = is_yelling(message);
+    let question = is_question(message);
+    let nothing = is_nothing(message);
+    // if yelling && question {
+    //     MessageType::YellingQuestion
+    // } else if yelling {
+    //     MessageType::Yelling
+    // } else if question {
+    //     MessageType::Question
+    // } else if nothing {
+    //     MessageType::Nothing
+    // } else {
+    //     MessageType::Other
+    // }
+    match (yelling, question, nothing) {
+        (true, true, _) => MessageType::YellingQuestion,
+        (true, _, _)    => MessageType::Yelling,
+        (_, true, _)    => MessageType::Question,
+        (_, _, true)    => MessageType::Nothing,
+        _               => MessageType::Other,
+    }
+}
+
+fn is_yelling(message: &str) -> bool {
+    message.chars().any(char::is_uppercase)
+        && !message.chars().any(char::is_lowercase)
+}
+
+fn is_question(message: &str) -> bool {
+    message.trim().ends_with("?")
+}
+
+fn is_nothing(message: &str) -> bool {
+    message.trim().is_empty()
+}
+
+enum MessageType {
+    Question,
+    Yelling,
+    YellingQuestion,
+    Nothing,
+    Other,
+}
