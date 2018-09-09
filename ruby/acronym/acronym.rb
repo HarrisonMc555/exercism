1168ee5cd0b411cddbed1fa1e8afd127f0f254f9
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-09 17:07:08 -0600

    Update ruby/acronym to use String.scan via suggestion

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,9 +1,6 @@
 # Create an  acronym for a phrase
 class Acronym
   def self.abbreviate(phrase)
-    clean_phrase = phrase.tr('-', ' ')
-    words = clean_phrase.split(' ')
-    first_letters = words.map { |w| w[0] }
-    first_letters.join('').upcase
+    phrase.scan(/\b\w/).join('').upcase
   end
 end
