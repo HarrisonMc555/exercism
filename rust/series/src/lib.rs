5302e7a9e05cc581d8152752a3fa4d96cfad5eaf
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-12 20:17:09 -0600

    Finish rust/series

---
+++ rust/series/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn series(digits: &str, len: usize) -> Vec<String> {
+    let char_vector = digits.chars().collect();
+    let char_windows = windows(char_vector, len);
+    char_windows.into_iter().map(|vec| join(&vec)).collect()
+}
+
+fn windows<T>(vec: Vec<T>, len: usize) -> Vec<Vec<T>>
+    where  T: Clone,
+{
+    if len > vec.len() {
+        return Vec::new();
+    }
+    let num_windows = vec.len() - len + 1;
+    (0..num_windows).map(|i| get_window(&vec, len, i)).collect()
+}
+
+fn get_window<T>(vec: &Vec<T>, len: usize, index: usize) -> Vec<T>
+    where T: Clone,
+{
+    vec[index..index + len].to_vec()
+}
+
+fn join(vec: &Vec<char>) -> String {
+    vec.into_iter().collect()
+}
