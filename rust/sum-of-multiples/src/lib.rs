8feeb254893d6d8b217bd3092dd1c093c4bc01a4
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-20 09:52:12 -0600

    Finish rust/sum-of-multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,9 +1,15 @@
-use std::collections::HashSet
+use std::collections::HashSet;
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut multiples = HashSet::new();
-    for factor in factors.iter() {
-        // Add all multiples of factor to `multiples`
+    for &factor in factors.iter() {
+        for multiple in get_multiples(limit, factor) {
+            multiples.insert(multiple);
+        }
     }
-    multiples.sum()
+    multiples.iter().sum()
+}
+
+fn get_multiples(limit: u32, factor: u32) -> std::iter::StepBy<std::ops::Range<u32>> {
+    (factor..limit).step_by(factor as usize)
 }
