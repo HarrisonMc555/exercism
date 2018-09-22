8588110dd9b5b83d3ddbb22b9e170edf68f3164c
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-22 16:27:14 -0600

    Start rust/diffie-hellman

---
+++ rust/diffie-hellman/src/lib.rs
@@ -0,0 +1,26 @@
+extern crate primes;
+
+use primes::PrimeSet;
+
+pub fn private_key(p: u64) -> u64 {
+    
+    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+}
+
+pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
+    unimplemented!(
+        "Calculate public key using prime numbers {} and {}, and private key {}",
+        p,
+        g,
+        a
+    )
+}
+
+pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
+    unimplemented!(
+        "Calculate secret key using prime number {}, public key {}, and private key {}",
+        p,
+        b_pub,
+        a
+    )
+}
