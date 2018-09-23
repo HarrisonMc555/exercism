91b51ad15e517ad1301e459a3719b29ccc3f354c
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-23 08:19:26 -0600

    Start rust/diffie-hellman

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,10 +1,19 @@
 extern crate primes;
+extern crate rand;
 
 use primes::PrimeSet;
+use rand::Rng;
+
+const NUM_PRIME_CHOICES: usize = 100;
 
 pub fn private_key(p: u64) -> u64 {
-    
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    let valid_primes: Vec<_> = PrimeSet::new().iter()
+        .take_while(|x| *x <= p).collect();
+    let close_primes: Vec<_> = valid_primes.iter()
+        .rev().take(NUM_PRIME_CHOICES).collect();
+    let choice_ref_ref = rand::thread_rng().choose(&close_primes)
+        .expect("Maximum range for private_key too small");
+    **choice_ref_ref
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
