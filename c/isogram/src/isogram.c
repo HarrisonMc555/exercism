a20dc9dca9521addcf448bd001f229640a9dacd4
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-27 11:33:02 -0600

    Update c/isogram to use uint32_t as set

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,26 +1,51 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdint.h>
+#include <stdio.h>
 
-/* This is a O(n^2) algorithm. It could be improved to O(n*log(n)), but that 
-   would require implementing a set in C, which I don't want to do right now.
-*/
+bool add_to_set32(uint32_t* set, int x) {
+    if (x < 0 || x >= 32) {
+        return false;
+    }
+    (*set) |= (1 << x);
+    return true;
+}
+
+bool set32_contains(uint32_t* set, int x) {
+    if (x < 0 || x >= 32) {
+        return false;
+    }
+    return (*set) & (1 << x);
+}
+
+int char_index(char c) {
+    if ('a' <= c && c <= 'z') {
+        return c - 'a';
+    }
+    if ('A' <= c && c <= 'Z') {
+        return c - 'A';
+    }
+    return -1;
+}
 
 bool is_isogram(const char phrase[]) {
     if (phrase == NULL) {
         return false;
     }
-    int i, j, phraselen;
+    uint32_t set = 0;
+    int i, phraselen;
     phraselen = strlen(phrase);
     for (i = 0; i < phraselen; i++) {
-        if (!isalpha(phrase[i])) {
+        char c = tolower(phrase[i]);
+        if (!isalpha(c)) {
             continue;
         }
-        for (j = 0; j < i; j++) {
-            if (tolower(phrase[i]) == tolower(phrase[j])) {
-                return false;
-            }
+        int c_index = char_index(c);
+        if (set32_contains(&set, c_index)) {
+            return false;
         }
+        add_to_set32(&set, c_index);
     }
     return true;
 }
