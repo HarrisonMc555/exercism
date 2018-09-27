172f3a38957cfa33fa707e684310ff9260954b40
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-27 14:32:43 -0600

    Finish rust/hamming

---
+++ rust/hamming/tests/hamming.rs
@@ -0,0 +1,36 @@
+extern crate hamming;
+
+#[test]
+fn test_no_difference_between_empty_strands() {
+    assert_eq!(hamming::hamming_distance("", ""), Some(0));
+}
+
+#[test]
+fn test_no_difference_between_identical_strands() {
+    assert_eq!(hamming::hamming_distance("GGACTGA", "GGACTGA"), Some(0));
+}
+
+#[test]
+fn test_complete_hamming_distance_in_small_strand() {
+    assert_eq!(hamming::hamming_distance("ACT", "GGA"), Some(3));
+}
+
+#[test]
+fn test_small_hamming_distance_in_the_middle_somewhere() {
+    assert_eq!(hamming::hamming_distance("GGACG", "GGTCG"), Some(1));
+}
+
+#[test]
+fn test_larger_distance() {
+    assert_eq!(hamming::hamming_distance("ACCAGGG", "ACTATGG"), Some(2));
+}
+
+#[test]
+fn test_first_string_is_longer() {
+    assert_eq!(hamming::hamming_distance("AAA", "AA"), None);
+}
+
+#[test]
+fn test_second_string_is_longer() {
+    assert_eq!(hamming::hamming_distance("A", "AA"), None);
+}
