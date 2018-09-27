14437bba5105fa9a6952f27ce29a6173c63551ac
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-27 17:05:17 -0600

    Finish rust/simple-linked-list

---
+++ rust/simple-linked-list/src/lib.rs
@@ -0,0 +1,174 @@
+pub struct SimpleLinkedList<T> {
+    head: Option<Box<Node<T>>>,
+    length: usize,
+}
+
+struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>>,
+}
+
+pub struct IntoIter<T>(SimpleLinkedList<T>);
+
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
+pub struct IterMut<'a, T: 'a>{
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<T> SimpleLinkedList<T> {
+    pub fn new() -> Self {
+        SimpleLinkedList {
+            head: None,
+            length: 0,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.length
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn push(&mut self, element: T) {
+        let new_node = Box::new(Node {
+            data: element,
+            next: self.head.take(),
+        });
+        self.head = Some(new_node);
+        self.length += 1;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        match self.head.take() {
+            None => None,
+            Some(node) => {
+                let node = *node;
+                self.head = node.next;
+                self.length -= 1;
+                Some(node.data)
+            }
+        }
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.data)
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_ref().map(|node| &**node)}
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut { next: self.head.as_mut().map(|node| &mut **node)}
+    }
+}
+
+impl<T: Clone> SimpleLinkedList<T> {
+    pub fn rev(&self) -> SimpleLinkedList<T> {
+        let mut new_list = Self::new();
+        let mut cur_link = self.head.as_ref();
+        while let Some(cur_node) = cur_link {
+            new_list.push(cur_node.data.clone());
+            cur_link = cur_node.next.as_ref();
+        }
+        new_list
+    }
+}
+
+impl<'a, T: Clone> From<&'a [T]> for SimpleLinkedList<T> {
+    fn from(_item: &[T]) -> Self {
+        let mut list = Self::new();
+        for e in _item {
+            list.push(e.clone());
+        }
+        list
+    }
+}
+
+impl<T> Into<Vec<T>> for SimpleLinkedList<T> {
+    fn into(self) -> Vec<T> {
+        let mut vec: Vec<_> = self.into_iter().collect();
+        vec.reverse();
+        vec
+    }
+}
+
+impl<T> Drop for SimpleLinkedList<T> {
+    fn drop(&mut self) {
+        let mut cur_link = self.head.take();
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = boxed_node.next.take();
+        }
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.data
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.data
+        })
+    }
+}
+
+impl<T> IntoIterator for SimpleLinkedList<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter(self)
+    }
+}
+
+impl<T> std::iter::FromIterator<T> for SimpleLinkedList<T> {
+    fn from_iter<U: IntoIterator<Item=T>>(iter: U) -> Self {
+        let mut list = SimpleLinkedList::new();
+        for i in iter {
+            list.push(i);
+        }
+        list
+    }
+}
+
+impl<T> std::fmt::Debug for SimpleLinkedList<T>
+where T: std::fmt::Debug {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut strings = vec![String::from("[")];
+        if !self.is_empty() {
+            strings.push(format!("{:?}", self.peek().unwrap()));
+        }
+        let mut iter = self.iter();
+        iter.next();
+        for e in iter {
+            strings.push(format!(", {:?}", e));
+        }
+        strings.push(String::from("]"));
+        write!(f, "{}", strings.join(""))
+    }
+}
