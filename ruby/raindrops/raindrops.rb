9c0d8d1e7eb6012da0b5a1d1bbe0aa132c25c9f6
Harrison McCullough <mccullough.harrison@gmail.com>
2018-10-15 10:16:33 -0600

    Improve ruby/raindrops by using hash instead of hard-coded

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,12 +1,17 @@
 # Create raindrop sounds for a number based on divisors
 class Raindrops
+  NUM_SOUND_PAIRS = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }.freeze
+
   def self.convert(num)
-    s = ''
-    s += 'Pling' if divisible?(num, 3)
-    s += 'Plang' if divisible?(num, 5)
-    s += 'Plong' if divisible?(num, 7)
-    s = num.to_s if s.empty?
-    s
+    sounds = NUM_SOUND_PAIRS
+             .select { |x, _| divisible?(num, x) }
+             .map { |_, sound| sound }
+             .join
+    sounds.empty? ? num.to_s : sounds
   end
 
   def self.divisible?(num, div)
