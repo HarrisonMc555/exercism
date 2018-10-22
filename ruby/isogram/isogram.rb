257c7ac92b3116bc1eb34ddb65533b7cca37ee13
Harrison McCullough <mccullough.harrison@gmail.com>
2018-10-21 20:46:06 -0600

    Improve ruby/isogram

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,9 +1,7 @@
-require 'set'
-
 # Determines whether a string is an isogram or not
 class Isogram
   def self.isogram?(str)
-    letters = str.delete('^a-zA-Z')
-    letters.length == letters.downcase.chars.to_set.length
+    letters = str.downcase.scan(/\w/)
+    letters.length == letters.uniq.length
   end
 end
