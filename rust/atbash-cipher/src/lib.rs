a367702721ea79f0782fe5a70f4da88d565d6cf0
Harrison McCullough <mccullough.harrison@gmail.com>
2018-11-07 14:59:10 -0700

    Reduce number of functions

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,5 +1,5 @@
 const ATBASH_LENGTH: u8 = 26;
-const ATBASH_START_CHAR: char = 'a';
+const ATBASH_START: u8 = 'a' as u8;
 const ATBASH_WORD_LENGTH: usize = 5;
 
 /// "Encipher" with the Atbash cipher.
@@ -30,9 +30,7 @@ fn atbash(string: &str, add_spaces: bool) -> String {
 
 fn switch_char(c: char) -> char {
     if c.is_ascii_alphabetic() {
-        let letter_index = to_letter_index(c);
-        let switched_letter_index = ATBASH_LENGTH - 1 - letter_index;
-        from_letter_index(switched_letter_index)
+        switch_letter(c)
     } else {
         c
     }
@@ -50,18 +48,8 @@ where
         }).collect()
 }
 
-fn to_letter_index(c: char) -> u8 {
-    to_u8(c.to_ascii_lowercase()) - to_u8(ATBASH_START_CHAR)
-}
-
-fn from_letter_index(index: u8) -> char {
-    from_u8(index + to_u8(ATBASH_START_CHAR))
-}
-
-fn to_u8(c: char) -> u8 {
-    c as u8
-}
-
-fn from_u8(u: u8) -> char {
-    u as char
+fn switch_letter(c: char) -> char {
+    let letter_index = c.to_ascii_lowercase() as u8 - ATBASH_START;
+    let switched_letter_index = ATBASH_LENGTH - 1 - letter_index;
+    (switched_letter_index + ATBASH_START) as char
 }
