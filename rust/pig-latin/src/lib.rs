b7601efd5e88e850207c0a39fff1b872a01a7967
Harrison McCullough <mccullough.harrison@gmail.com>
2018-11-08 10:17:39 -0700

    Add logic to translate entire sentences
    Also renamed some constants.

--- rust/pig-latin/src/lib.rs
+++ rust/pig-latin/src/lib.rs
@@ -1,15 +1,23 @@
-const DIAGRAPH_PATTERNS: [&'static str; 6] = ["ch", "qu", "thr", "th", "sch", "rh"];
-const DIAGRAPH_AFTER_VOWEL_PATTERNS: [&'static str; 1] = ["qu"];
+const DIAGRAPHS: [&'static str; 6] = ["ch", "qu", "thr", "th", "sch", "rh"];
+const DIAGRAPHS_AFTER_VOWEL: [&'static str; 1] = ["qu"];
 const VOWEL_DIAGRAPHS: [&'static str; 1] = ["xr"];
 
 pub fn translate(input: &str) -> String {
-    if input.is_empty() {
-        return String::from(input);
+    input
+        .split(" ")
+        .map(|word| translate_word(word))
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+fn translate_word(word: &str) -> String {
+    if word.is_empty() {
+        return String::from(word);
     }
-    if starts_with_vowel(input) {
-        translate_word_starting_with_vowel(input)
+    if starts_with_vowel(word) {
+        translate_word_starting_with_vowel(word)
     } else {
-        translate_word_starting_with_consonant(input)
+        translate_word_starting_with_consonant(word)
     }
 }
 
@@ -31,14 +39,14 @@ fn get_first(word: &str) -> String {
 }
 
 fn get_diagraph(word: &str) -> Option<String> {
-    DIAGRAPH_PATTERNS
+    DIAGRAPHS
         .iter()
         .find(|&diagraph| word.starts_with(diagraph))
         .map(|&word| String::from(word))
 }
 
 fn get_diagraph_after_vowel(word: &str) -> Option<String> {
-    let diagraph = DIAGRAPH_AFTER_VOWEL_PATTERNS.iter().find(|&diagraph| {
+    let diagraph = DIAGRAPHS_AFTER_VOWEL.iter().find(|&diagraph| {
         word.chars()
             .skip(1)
             .collect::<String>()
@@ -55,7 +63,7 @@ fn starts_with_vowel(word: &str) -> bool {
     }
     let first_letter = word.chars().next().unwrap();
     // Regular vowels are vowels
-    if is_vowel(first_letter) {
+    if is_regular_vowel(first_letter) {
         return true;
     }
     // Vowel diagraphs count as vowels
@@ -72,12 +80,12 @@ fn starts_with_vowel(word: &str) -> bool {
     // 'y' is only a vowel if followed by a consonant
     let second_letter = word.chars().skip(1).next();
     match second_letter {
-        Some(c) => is_consonant(c),
+        Some(c) => is_regular_consonant(c),
         None => true,
     }
 }
 
-fn is_vowel(letter: char) -> bool {
+fn is_regular_vowel(letter: char) -> bool {
     let letter = letter.to_ascii_lowercase();
     match letter {
         'a' => true,
@@ -89,6 +97,6 @@ fn is_vowel(letter: char) -> bool {
     }
 }
 
-fn is_consonant(letter: char) -> bool {
-    !is_vowel(letter)
+fn is_regular_consonant(letter: char) -> bool {
+    !is_regular_vowel(letter)
 }
--- rust/pig-latin/tests/pig-latin.rs
+++ rust/pig-latin/tests/pig-latin.rs
@@ -101,7 +101,6 @@ fn test_y_is_treated_like_a_vowel_at_the_end_of_a_consonant_cluster() {
 }
 
 #[test]
-#[ignore]
 fn test_a_whole_phrase() {
     assert_eq!(pl::translate("quick fast run"), "ickquay astfay unray");
 }
