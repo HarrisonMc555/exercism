e1b79627337d56bd9c25576989e76dafc335a677
Harrison McCullough <mccullough.harrison@gmail.com>
2018-11-09 15:29:08 -0700

    Cleaned up a lot of code

--- rust/variable-length-quantity/src/lib.rs
+++ rust/variable-length-quantity/src/lib.rs
@@ -13,29 +13,28 @@ pub enum Error {
 /// Convert a list of numbers to a stream of bytes encoded with variable length
 /// encoding.
 pub fn to_bytes(values: &[u32]) -> Vec<u8> {
-    values.iter().flat_map(|x| u32_to_bytes(x)).collect()
+    values.iter().flat_map(|x| value_to_bytes(x)).collect()
 }
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
 pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
     let mut result = Vec::new();
     let mut bytes = bytes;
-    let mut value_bytes = get_value_bytes(bytes)?;
-    while !value_bytes.is_empty() {
-        result.extend(decode_value(value_bytes));
+    while !bytes.is_empty() {
+        let value_bytes = get_value_bytes(bytes)?;
+        result.push(decode_value(value_bytes));
         bytes = &bytes[value_bytes.len()..];
-        value_bytes = get_value_bytes(bytes)?;
     }
     Ok(result)
 }
 
-fn u32_to_bytes(value: &u32) -> Vec<u8> {
-    if value == &0 {
-        return vec![0];
-    }
-    let (first, rest) = encode_first_byte(value);
-    let mut rest = rest;
-    let mut bytes = vec![first];
+///////////////////////////////////////////////////////////////////////////////
+// Encoding helper functions
+///////////////////////////////////////////////////////////////////////////////
+
+fn value_to_bytes(value: &u32) -> Vec<u8> {
+    let (last, mut rest) = encode_last_byte(value);
+    let mut bytes = vec![last];
     while rest > 0 {
         let (byte, next_rest) = encode_other_byte(&rest);
         bytes.push(byte);
@@ -45,49 +44,45 @@ fn u32_to_bytes(value: &u32) -> Vec<u8> {
     bytes
 }
 
-fn encode_first_byte(value: &u32) -> (u8, u32) {
+fn encode_last_byte(value: &u32) -> (u8, u32) {
+    extract_bits(value)
+}
+
+fn encode_other_byte(value: &u32) -> (u8, u32) {
+    let (byte, rest) = extract_bits(value);
+    (byte | CONTINUE_BIT, rest)
+}
+
+fn extract_bits(value: &u32) -> (u8, u32) {
     let byte = (value & MASK) as u8;
     let rest = value >> MASK_LENGTH;
     (byte, rest)
 }
 
-fn encode_other_byte(value: &u32) -> (u8, u32) {
-    let byte = (value & MASK) as u8;
-    let rest = value >> MASK_LENGTH;
-    (byte | CONTINUE_BIT, rest)
-}
+///////////////////////////////////////////////////////////////////////////////
+// Decoding helper functions
+///////////////////////////////////////////////////////////////////////////////
 
-fn decode_value(bytes: &[u8]) -> Result<u32, Error> {
-    let other_bytes = &bytes[..bytes.len() - 1];
-    let first_byte = bytes[bytes.len() - 1];
+fn decode_value(bytes: &[u8]) -> u32 {
+    let last_index = bytes.len() - 1;
+    let other_bytes = &bytes[..last_index];
+    let last_byte = bytes[last_index];
     let mut result: u32 = 0;
     for byte in other_bytes {
         result <<= MASK_LENGTH;
         result |= decode_other_byte(byte);
     }
     result <<= MASK_LENGTH;
-    result |= decode_last_byte(&first_byte);
-    Ok(result)
+    result | decode_last_byte(&last_byte)
 }
 
 fn get_value_bytes(bytes: &[u8]) -> Result<&[u8], Error> {
-    if bytes.is_empty() {
-        return Ok(&[]);
-    }
-    if let Some(index) = bytes
-        .iter()
-        .take(MAX_BYTES)
-        .position(|byte| is_last_byte(byte))
-    {
-        if index + 1 == MAX_BYTES
-            && decode_other_byte(&bytes[0]) > HIGHEST_BYTE_MAX_VALUE as u32
-        {
-            Err(Error::Overflow)
-        } else {
-            Ok(&bytes[..index + 1])
-        }
+    let length = first_value_length(bytes)?;
+    let bytes = &bytes[..length];
+    if is_overflow(bytes) {
+        Err(Error::Overflow)
     } else {
-        Err(Error::IncompleteNumber)
+        Ok(bytes)
     }
 }
 
@@ -105,5 +100,19 @@ fn decode_last_byte(byte: &u8) -> u32 {
 }
 
 fn decode_other_byte(byte: &u8) -> u32 {
-    (byte & !CONTINUE_BIT) as u32
+    (byte ^ CONTINUE_BIT) as u32
+}
+
+fn first_value_length(bytes: &[u8]) -> Result<usize, Error> {
+    bytes
+        .iter()
+        .take(MAX_BYTES)
+        .position(|byte| is_last_byte(byte))
+        .map(|index| index + 1)
+        .ok_or(Error::IncompleteNumber)
+}
+
+fn is_overflow(bytes: &[u8]) -> bool {
+    bytes.len() == MAX_BYTES
+        && decode_other_byte(&bytes[0]) > HIGHEST_BYTE_MAX_VALUE as u32
 }
