f270c392ee9d103a052a1c144fe13c48324dab38
Harrison McCullough <mccullough.harrison@gmail.com>
2018-11-10 22:12:13 -0700

    Add lazy static hash for brackets

--- rust/bracket-push/src/lib.rs
+++ rust/bracket-push/src/lib.rs
@@ -1,3 +1,18 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref BRACKET_HASH: HashMap<char, char> = {
+        let mut bracket_hash = HashMap::new();
+        bracket_hash.insert('(', ')');
+        bracket_hash.insert('[', ']');
+        bracket_hash.insert('{', '}');
+        bracket_hash
+    };
+}
+
 pub fn brackets_are_balanced(string: &str) -> bool {
     unimplemented!("Check if the string \"{}\" contains balanced brackets", string);
 }
