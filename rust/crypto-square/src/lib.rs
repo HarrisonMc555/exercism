fc251446786bf66663e0339d6c994fe4baae5300
Harrison McCullough <mccullough.harrison@gmail.com>
2018-11-10 23:22:49 -0700

    Add normalize function

--- rust/crypto-square/src/lib.rs
+++ rust/crypto-square/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn encrypt(input: &str) -> String {
+    let input = normalize(input);
     unimplemented!("Encrypt {:?} using a square code", input)
+}
+
+fn normalize(input: &str) -> String {
+    input
+        .chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .map(|c| c.to_ascii_lowercase())
+        .collect()
 }
