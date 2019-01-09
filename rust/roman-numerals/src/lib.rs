4d7b9c0692b02ac318be86d8562d71c2aa731140
Harrison McCullough <mccullough.harrison@gmail.com>
2019-01-08 20:48:22 -0700

    Add a fairly detalied implementation for I

--- rust/roman-numerals/src/lib.rs
+++ rust/roman-numerals/src/lib.rs
@@ -1,15 +1,53 @@
 use std::fmt::{Display, Formatter, Result};
 
-pub struct Roman;
+const NUM_ROMAN_SYMBOLS: usize = 1;
+
+const ROMAN_VALUE_SYMBOL_PAIRS: [(u32, char); NUM_ROMAN_SYMBOLS] = [
+    (1, 'I'),
+];
+
+pub struct Roman {
+    symbol_counts: [usize; NUM_ROMAN_SYMBOLS],
+}
 
 impl Display for Roman {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> Result {
-        unimplemented!("Return a roman-numeral string representation of the Roman object");
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let mut result = Vec::new();
+        let symbols = ROMAN_VALUE_SYMBOL_PAIRS.iter()
+            .map(|&(_, symbol)| symbol);
+        for (symbol, &count) in symbols.zip(self.symbol_counts.iter()) {
+            result.push(multiply_char(symbol, count));
+        }
+        result.reverse();
+        let string = result.join("");
+        write!(f, "{}", string)
     }
 }
 
 impl From<u32> for Roman {
     fn from(num: u32) -> Self {
-        unimplemented!("Construct a Roman object from the '{}' number", num);
+        let mut num = num;
+        let mut symbol_counts = [0; NUM_ROMAN_SYMBOLS];
+        for i in 0..NUM_ROMAN_SYMBOLS {
+            let (roman_value, _) = ROMAN_VALUE_SYMBOL_PAIRS[i];
+            symbol_counts[i] = get_count(&mut num, roman_value);
+        }
+        assert!(num == 0);
+        Roman {
+            symbol_counts,
+        }
     }
+}
+
+fn get_count(num: &mut u32, multiple: u32) -> usize {
+    let mut count = 0;
+    while *num >= multiple {
+        *num -= multiple;
+        count += 1;
+    }
+    count
+}
+
+fn multiply_char(c: char, num: usize) -> String {
+    (0..num).map(|_| c).collect()
 }
