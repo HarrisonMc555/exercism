ca0e1403bcb7ad1a746a6d9e329dfc691fabefcc
Harrison McCullough <mccullough.harrison@gmail.com>
2019-01-22 11:21:33 -0700

    Update rust/two-bucket

--- rust/two-bucket/src/lib.rs
+++ rust/two-bucket/src/lib.rs
@@ -33,7 +33,7 @@ pub fn solve(
     start_bucket: &Bucket,
 ) -> BucketStats {
     let zero_state = (0, 0);
-    let first_state = match &start_bucket {
+    let first_state = match start_bucket {
         Bucket::One => (capacity_1, 0),
         Bucket::Two => (0, capacity_2),
     };
@@ -48,32 +48,28 @@ pub fn solve(
         if let Some(state) = goal_bucket(&cur_states, goal) {
             return BucketStats {
                 moves: num_moves,
-                goal_bucket: get_goal_bucket(&state, goal),
-                other_bucket: get_other_capacity(&state, goal),
+                goal_bucket: get_goal_bucket(state, goal),
+                other_bucket: get_other_capacity(state, goal),
             };
         }
         prev_states.extend(&cur_states);
-        cur_states = all_next_moves(&cur_states, &limit, &prev_states);
-        cur_states = remove_opposite_start(cur_states, &limit, &start_bucket);
+        cur_states = all_next_moves(&cur_states, limit, &prev_states);
+        cur_states = remove_opposite_start(cur_states, limit, start_bucket);
         num_moves += 1;
     }
 }
 
 fn all_next_moves(
     states: &HashSet<State>,
-    limit: &Limit,
+    limit: Limit,
     prev_states: &HashSet<State>,
 ) -> HashSet<State> {
-    states
-        .iter()
-        .cloned()
-        .flat_map(|st| next_moves(&st, limit, prev_states))
-        .collect()
+    states.iter().cloned().flat_map(|st| next_moves(st, limit, prev_states)).collect()
 }
 
 fn next_moves(
-    state: &State,
-    limit: &Limit,
+    state: State,
+    limit: Limit,
     prev_states: &HashSet<State>,
 ) -> HashSet<State> {
     possible_moves(state, limit)
@@ -82,16 +78,16 @@ fn next_moves(
         .collect()
 }
 
-fn possible_moves(state: &State, limit: &Limit) -> HashSet<State> {
+fn possible_moves(state: State, limit: Limit) -> HashSet<State> {
     let mut moves = pouring_moves(state, limit);
     moves.extend(emptying_moves(state, limit));
     moves.extend(filling_moves(state, limit));
     moves
 }
 
-fn pouring_moves(state: &State, limit: &Limit) -> HashSet<State> {
-    let &(a, b) = state;
-    let &(a_max, b_max) = limit;
+fn pouring_moves(state: State, limit: Limit) -> HashSet<State> {
+    let (a, b) = state;
+    let (a_max, b_max) = limit;
     let mut moves = HashSet::new();
     if a < a_max {
         let a_new = cmp::min(a_max, a + b);
@@ -108,8 +104,8 @@ fn pouring_moves(state: &State, limit: &Limit) -> HashSet<State> {
     moves
 }
 
-fn emptying_moves(state: &State, _limit: &Limit) -> HashSet<State> {
-    let &(a, b) = state;
+fn emptying_moves(state: State, _limit: Limit) -> HashSet<State> {
+    let (a, b) = state;
     let mut moves = HashSet::new();
     if a > 0 {
         moves.insert((0, b));
@@ -120,9 +116,9 @@ fn emptying_moves(state: &State, _limit: &Limit) -> HashSet<State> {
     moves
 }
 
-fn filling_moves(state: &State, limit: &Limit) -> HashSet<State> {
-    let &(a, b) = state;
-    let &(a_max, b_max) = limit;
+fn filling_moves(state: State, limit: Limit) -> HashSet<State> {
+    let (a, b) = state;
+    let (a_max, b_max) = limit;
     let mut moves = HashSet::new();
     if a < a_max {
         moves.insert((a_max, b));
@@ -134,16 +130,19 @@ fn filling_moves(state: &State, limit: &Limit) -> HashSet<State> {
 }
 
 fn goal_bucket(states: &HashSet<State>, goal: Capacity) -> Option<State> {
-    states.iter().find(|&st| has_goal_bucket(st, goal)).cloned()
+    states
+        .iter()
+        .find(|&&st| has_goal_bucket(st, goal))
+        .cloned()
 }
 
-fn has_goal_bucket(state: &State, goal: Capacity) -> bool {
-    let &(a, b) = state;
+fn has_goal_bucket(state: State, goal: Capacity) -> bool {
+    let (a, b) = state;
     a == goal || b == goal
 }
 
-fn get_goal_bucket(state: &State, goal: Capacity) -> Bucket {
-    let &(a, b) = state;
+fn get_goal_bucket(state: State, goal: Capacity) -> Bucket {
+    let (a, b) = state;
     if a == goal {
         Bucket::One
     } else if b == goal {
@@ -153,8 +152,8 @@ fn get_goal_bucket(state: &State, goal: Capacity) -> Bucket {
     }
 }
 
-fn get_other_capacity(state: &State, goal: Capacity) -> Capacity {
-    let &(a, b) = state;
+fn get_other_capacity(state: State, goal: Capacity) -> Capacity {
+    let (a, b) = state;
     if a == goal {
         b
     } else if b == goal {
@@ -166,23 +165,23 @@ fn get_other_capacity(state: &State, goal: Capacity) -> Capacity {
 
 fn remove_opposite_start(
     states: HashSet<State>,
-    limit: &Limit,
+    limit: Limit,
     start_bucket: &Bucket,
 ) -> HashSet<State> {
     states
         .iter()
-        .filter(|&st| !is_opposite_start(st, &limit, &start_bucket))
+        .filter(|&&st| !is_opposite_start(st, limit, &start_bucket))
         .cloned()
         .collect()
 }
 
 fn is_opposite_start(
-    state: &State,
-    limit: &Limit,
+    state: State,
+    limit: Limit,
     start_bucket: &Bucket,
 ) -> bool {
-    let &(a, b) = state;
-    let &(a_max, b_max) = limit;
+    let (a, b) = state;
+    let (a_max, b_max) = limit;
     match start_bucket {
         Bucket::One => a == 0 && b == b_max,
         Bucket::Two => a == a_max && b == 0,
