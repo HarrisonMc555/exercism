3499e25f2efbbc3451785bf1dac4f5b1c525b1e2
Harrison McCullough <mccullough.harrison@gmail.com>
2019-02-12 12:22:58 -0700

    Continue rust/grep

--- rust/grep/src/lib.rs
+++ rust/grep/src/lib.rs
@@ -1,25 +1,29 @@
 use failure::Error;
 
-/// While using raw slice of str to handle flags is convenient,
-/// in the real-world projects it is customary to use a struct,
-/// that contains flags-related logic. So in this exercise
-/// we ask you to implement a custom struct.
+/// While using raw slice of str to handle flags is convenient, in the
+/// real-world projects it is customary to use a struct, that contains
+/// flags-related logic. So in this exercise we ask you to implement a custom
+/// struct.
 ///
-/// If you are curious about real-world implementation, refer to the `clap-rs` crate:
+/// If you are curious about real-world implementation, refer to the `clap-rs`
+/// crate:
 /// https://github.com/kbknapp/clap-rs/blob/master/src/args/arg_matches.rs
+
 #[derive(Debug)]
 pub struct Flags;
 
 impl Flags {
-    pub fn new(flags: &[&str]) -> Self {
-        unimplemented!(
-            "Given the flags {:?} implement your own 'Flags' struct to handle flags-related logic",
-            flags
-        );
+    pub fn new(_flags: &[&str]) -> Self {
+        Flags
     }
 }
 
-pub fn grep(pattern: &str, flags: &Flags, files: &[&str]) -> Result<Vec<String>, Error> {
+pub fn grep(
+    pattern: &str,
+    flags: &Flags,
+    files: &[&str],
+) -> Result<Vec<String>, Error> {
+    let matches = Vec::new();
     unimplemented!(
         "Search the files '{:?}' for '{}' pattern and save the matches in a vector. Your search logic should be aware of the given flags '{:?}'",
         files,
