80c9541024fd37b9014f8d1671a58a1fcb68328f
Harrison McCullough <mccullough.harrison@gmail.com>
2019-02-21 20:25:22 -0700

    Pass all tests for rust/book-store

--- rust/book-store/tests/book-store.rs
+++ rust/book-store/tests/book-store.rs
@@ -77,7 +77,6 @@ fn test_two_groups_of_four_is_cheaper_than_group_of_five_plus_group_of_three() {
 }
 
 #[test]
-#[ignore]
 /// Group of four plus group of two is cheaper than two groups of three
 fn test_group_of_four_plus_group_of_two_is_cheaper_than_two_groups_of_three() {
     process_total_case(
@@ -87,7 +86,6 @@ fn test_group_of_four_plus_group_of_two_is_cheaper_than_two_groups_of_three() {
 }
 
 #[test]
-#[ignore]
 /// Two each of first 4 books and 1 copy each of rest
 fn test_two_each_of_first_4_books_and_1_copy_each_of_rest() {
     process_total_case(
@@ -100,7 +98,6 @@ fn test_two_each_of_first_4_books_and_1_copy_each_of_rest() {
 }
 
 #[test]
-#[ignore]
 /// Two copies of each book
 fn test_two_copies_of_each_book() {
     process_total_case(
@@ -113,7 +110,6 @@ fn test_two_copies_of_each_book() {
 }
 
 #[test]
-#[ignore]
 /// Three copies of first book and 2 each of remaining
 fn test_three_copies_of_first_book_and_2_each_of_remaining() {
     process_total_case(
@@ -126,7 +122,6 @@ fn test_three_copies_of_first_book_and_2_each_of_remaining() {
 }
 
 #[test]
-#[ignore]
 /// Three each of first 2 books and 2 each of remaining books
 fn test_three_each_of_first_2_books_and_2_each_of_remaining_books() {
     process_total_case(
@@ -139,7 +134,6 @@ fn test_three_each_of_first_2_books_and_2_each_of_remaining_books() {
 }
 
 #[test]
-#[ignore]
 /// Four groups of four are cheaper than two groups each of five and three
 fn test_four_groups_of_four_are_cheaper_than_two_groups_each_of_five_and_three() {
     process_total_case(
