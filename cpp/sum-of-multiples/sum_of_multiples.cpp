86ba147472ea90d71ae89c5ca8a420e051e8c74f
Harrison McCullough <mccullough.harrison@gmail.com>
2019-02-23 13:35:35 -0700

    Update cpp/sum-of-multiples

--- cpp/sum-of-multiples/sum_of_multiples.cpp
+++ cpp/sum-of-multiples/sum_of_multiples.cpp
@@ -1,18 +1,21 @@
 #include "sum_of_multiples.h"
+#include <unordered_set>
 
 int sum_of_multiples::to(const std::vector<int> factors, int max_num) {
-    int sum = 0;
-    for (int i = 0; i < max_num; i++) {
-        bool is_multiple = false;
-        for (int factor : factors) {
-            if (i % factor == 0) {
-                is_multiple = true;
-                break;
-            }
-        }
-        if (is_multiple) {
-            sum += i;
+    std::unordered_set<int> multiples;
+
+    for (int factor : factors) {
+        int multiple = factor;
+        while (multiple < max_num) {
+            multiples.insert(multiple);
+            multiple += factor;
         }
     }
+
+    int sum = 0;
+    for (int multiple : multiples) {
+        sum += multiple;
+    }
+
     return sum;
 }
