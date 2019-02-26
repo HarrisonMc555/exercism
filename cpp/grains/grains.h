7ccc13bcbf582d96fde8aea75f35f3c913243b23
Harrison McCullough <mccullough.harrison@gmail.com>
2019-02-26 10:14:36 -0700

    Use cstdint, constexpr, and header guards

--- cpp/grains/grains.h
+++ cpp/grains/grains.h
@@ -1,7 +1,22 @@
-typedef unsigned long long u64;
-typedef unsigned int u32;
+#ifndef GRAINS_H
+#define GRAINS_H
+
+#include <cstdint>
 
 namespace grains {
-    u64 square(u32 square_index);
-    u64 total();
+    constexpr uint64_t square(uint32_t square_index) {
+        return 1ULL << (square_index - 1);
+    }
+
+    constexpr uint64_t total() {
+        uint64_t result = 0ULL;
+        const uint32_t BOARD_WITH = 8;
+        const uint32_t NUM_SQUARES = BOARD_WITH * BOARD_WITH;
+        for (uint32_t i = 0; i < NUM_SQUARES; i++) {
+            result += square(i);
+        }
+        return result;
+    }
 }
+
+#endif
