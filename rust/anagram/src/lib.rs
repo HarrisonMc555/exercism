bb3ae87a81c4555a4561501dbf0f5922a54b0459
Harrison McCullough <mccullough.harrison@gmail.com>
2019-03-18 13:45:52 -0600

    Start rust/anagram

---
+++ rust/anagram/src/lib.rs
@@ -0,0 +1,28 @@
+use counter::Counter;
+use std::collections::HashSet;
+
+pub fn anagrams_for<'a>(
+    word: &str,
+    possible_anagrams: &'a [&str],
+) -> HashSet<&'a str> {
+    let word = word.to_lowercase();
+    let word_counter = letter_counter(&word);
+    possible_anagrams
+        .into_iter()
+        .filter(|possibility| is_anagram(possibility, &word, &word_counter))
+        .cloned() // Just cloning the reference
+        .collect()
+}
+
+fn is_anagram(
+    possibility: &str,
+    word: &str,
+    word_counter: &Counter<char>,
+) -> bool {
+    let possibility = possibility.to_lowercase();
+    possibility != word && letter_counter(&possibility) == *word_counter
+}
+
+fn letter_counter(word: &str) -> Counter<char> {
+    word.to_lowercase().chars().collect::<Counter<_>>()
+}
