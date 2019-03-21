a618b479a133d9add8afe6feecb1fe40fd1a18b4
Harrison McCullough <mccullough.harrison@gmail.com>
2019-03-21 15:01:34 -0600

    More work on rust/ocr-numbers

--- rust/ocr-numbers/src/lib.rs
+++ rust/ocr-numbers/src/lib.rs
@@ -12,13 +12,35 @@ pub enum Error {
 }
 
 pub fn convert(input: &str) -> Result<String, Error> {
+    let lines: DigitVec =
+        input.lines().map(|line| line.chars().collect()).collect();
+    let rem = lines.len() % LargeChar::NUM_ROWS;
+    if rem != 0 {
+        return Err(Error::InvalidRowCount(rem));
+    }
+    let strings = lines
+        .chunks(LargeChar::NUM_ROWS)
+        // .map(|rows| convert_line(&rows.to_vec()));
+        .map(convert_line);
+
     unimplemented!("Convert the input '{}' to a string", input);
 }
 
-fn convert_digit(input: &DigitVec) -> Option<char> {
-    DIGIT_MAP
-        .get(&LargeChar::try_parse(input)?)
+fn convert_line(rows: &DigitVec) -> Result<String, Error> {
+    let num_cols = rows[0].len();
+    if let Some(invalid_length) =
+        rows.iter().map(Vec::len).find(|len| *len != num_cols)
+    {
+        return Err(Error::InvalidColumnCount(invalid_length));
+    }
+    Ok("".to_string())
+}
+
+fn convert_digit(rows: &DigitVec) -> Result<char, Error> {
+    Ok(DIGIT_MAP
+        .get(&LargeChar::try_parse(rows)?)
         .map(Clone::clone)
+        .unwrap_or('?'))
 }
 
 lazy_static! {
@@ -50,29 +72,27 @@ lazy_static! {
 
 #[derive(Debug, Hash, Clone, Eq, PartialEq, Ord, PartialOrd)]
 struct LargeChar {
-    lines: Vec<Vec<char>>,
+    rows: Vec<Vec<char>>,
 }
 
 impl LargeChar {
-    pub fn try_parse(lines: &DigitVec) -> Option<Self> {
-        if !LargeChar::is_valid(lines) {
-            return None;
-        }
-        let lc = LargeChar {
-            lines: lines.to_vec(),
-        };
-        Some(lc)
-    }
-
     const NUM_ROWS: usize = 4;
     const NUM_COLS: usize = 3;
-    fn is_valid(lines: &DigitVec) -> bool {
-        if lines.len() != LargeChar::NUM_ROWS {
-            return false;
+
+    pub fn try_parse(rows: &DigitVec) -> Result<Self, Error> {
+        if rows.len() != LargeChar::NUM_ROWS {
+            return Err(Error::InvalidRowCount(rows.len()));
         }
-        if !lines.iter().all(|row| row.len() == LargeChar::NUM_COLS) {
-            return false;
+        if let Some(col_count) = rows
+            .iter()
+            .map(Vec::len)
+            .find(|row_len| *row_len != LargeChar::NUM_COLS)
+        {
+            return Err(Error::InvalidColumnCount(col_count));
         }
-        true
+        let lc = LargeChar {
+            rows: rows.to_vec(),
+        };
+        Ok(lc)
     }
 }
