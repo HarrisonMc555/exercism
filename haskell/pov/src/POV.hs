99ddb14e586c5a4fb01185df3b1ac3640e9bb79e
Harrison McCullough <mccullough.harrison@gmail.com>
2019-05-06 10:33:01 -0600

    Always return input tree for fromPOV

--- haskell/pov/src/POV.hs
+++ haskell/pov/src/POV.hs
@@ -3,7 +3,7 @@ module POV (fromPOV, tracePathBetween) where
 import Data.Tree (Tree)
 
 fromPOV :: Eq a => a -> Tree a -> Maybe (Tree a)
-fromPOV x tree = error "You need to implement this function."
+fromPOV x tree = Just tree
 
 tracePathBetween :: Eq a => a -> a -> Tree a -> Maybe [a]
 tracePathBetween from to tree = error "You need to implement this function."
