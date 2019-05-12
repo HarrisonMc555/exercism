f7bc9b29bdbbe5c415bb83a3371c4a820302fe67
Harrison McCullough <mccullough.harrison@gmail.com>
2019-05-12 12:49:56 -0600

    Finish clojure/nucleotide-count

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,16 +1,17 @@
 (ns nucleotide-count
   (:require [clojure.string :as string]))
 
-(defn- in-string? [char string]
-  (some? (string/index-of string char)))
+(def ^:private empty-nucleotides {\A 0 \C 0 \T 0 \G 0})
 
 (defn count [nucleotide strand]
-  (if (not (in-string? nucleotide "AGCT"))
+  (if-not (contains? empty-nucleotides nucleotide)
     (throw (IllegalArgumentException. "Invalid nucleotide"))
     (->> strand
          (filter #(= nucleotide %1))
          (clojure.core/count))))
 
 (defn nucleotide-counts [strand]
-  
-)
+  (let [inc-or-throw #(if-not (contains? %1 %2)
+                         (throw (Exception. "Invalid nucleotide"))
+                         (update %1 %2 inc))]
+    (reduce inc-or-throw empty-nucleotides strand)))
