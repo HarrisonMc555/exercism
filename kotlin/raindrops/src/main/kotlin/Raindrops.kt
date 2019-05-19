29f62ceb2da30411b87c59e18510239812bcc0f6
Harrison McCullough <mccullough.harrison@gmail.com>
2019-05-18 22:53:57 -0600

    Finish kotlin/raindrops

---
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -0,0 +1,18 @@
+object Raindrops {
+  fun convert(input: Int): String {
+    val factor_sounds = arrayOf(
+      Pair(3, "Pling"),
+      Pair(5, "Plang"),
+      Pair(7, "Plong")
+    )
+    val to_keep = factor_sounds.filter { (x, _) -> input % x == 0 }
+    val sounds = to_keep.map { (_, s) -> s }
+    val combined = sounds.joinToString(separator = "")
+    if (combined == "") {
+      return input.toString()
+    } else {
+      return combined
+    }
+  }
+}
+
