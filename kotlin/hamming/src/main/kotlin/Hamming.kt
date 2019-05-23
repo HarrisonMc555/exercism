3a574faf9e48fe1d858d164f671ee1a1c8f71bdc
Harrison McCullough <mccullough.harrison@gmail.com>
2019-05-23 14:06:48 -0600

    Update kotlin/hamming to use require

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,7 +1,7 @@
 object Hamming {
   fun compute(leftStrand: String, rightStrand: String): Int {
-    if (leftStrand.length != rightStrand.length) {
-      throw IllegalArgumentException("left and right strands must be of equal length.")
+    require (leftStrand.length == rightStrand.length) {
+      "left and right strands must be of equal length."
     }
     return leftStrand.zip(rightStrand).count { (left, right) -> left != right }
   }
