42b956c8d969d770a6cf8f020a74b586e952e47b
Harrison McCullough <mccullough.harrison@gmail.com>
2019-05-24 06:04:10 -0600

    Finish java/scrabble-score

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,27 +1,59 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Scrabble {
 
   private String word;
   private int score;
 
-  // A, E, I, O, U, L, N, R, S, T       1
-  // D, G                               2
-  // B, C, M, P                         3
-  // F, H, V, W, Y                      4
-  // K                                  5
-  // J, X                               8
-  // Q, Z                               10
+  static final Map<Character, Integer> letterToScore;
+
+  static {
+    letterToScore = new HashMap<Character, Integer>();
+    letterToScore.put('A', 1);
+    letterToScore.put('E', 1);
+    letterToScore.put('I', 1);
+    letterToScore.put('O', 1);
+    letterToScore.put('U', 1);
+    letterToScore.put('L', 1);
+    letterToScore.put('N', 1);
+    letterToScore.put('R', 1);
+    letterToScore.put('S', 1);
+    letterToScore.put('T', 1);
+    letterToScore.put('D', 2);
+    letterToScore.put('G', 2);
+    letterToScore.put('B', 3);
+    letterToScore.put('C', 3);
+    letterToScore.put('M', 3);
+    letterToScore.put('P', 3);
+    letterToScore.put('F', 4);
+    letterToScore.put('H', 4);
+    letterToScore.put('V', 4);
+    letterToScore.put('W', 4);
+    letterToScore.put('Y', 4);
+    letterToScore.put('K', 5);
+    letterToScore.put('J', 8);
+    letterToScore.put('X', 8);
+    letterToScore.put('Q', 10);
+    letterToScore.put('Z', 10);
+  }
 
   Scrabble(String word) {
-    throw new UnsupportedOperationException(
-        "Delete this statement and write your own implementation.");
+    this.word = word;
+    this.score = getScore(this.word);
   }
 
   int getScore() {
-    throw new UnsupportedOperationException(
-        "Delete this statement and write your own implementation.");
+    return this.score;
   }
 
   static int getScore(String word) {
-    return -1;
+    word = word.toUpperCase();
+    int score = 0;
+    for (int i = 0; i < word.length(); i++) {
+      char c = word.charAt(i);
+      score += letterToScore.get(c);
+    }
+    return score;
   }
 }
