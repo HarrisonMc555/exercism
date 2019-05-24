196d491f61d55cc5c00adfc0ef46e5a019ec6147
Harrison McCullough <mccullough.harrison@gmail.com>
2019-05-24 08:43:20 -0600

    Finish java/darts

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,11 +1,46 @@
 class Darts {
 
-    Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+  private Point dartPoint;
+  private int _score;
+
+  static final Point center = new Point(0, 0);
+  static final double outerRadius = 10, middleRadius = 5, innerRadius = 1;
+  static final int noTargetPoints = 0, outerPoints = 1, middlePoints = 5, innerPoints = 10;
+
+  Darts(double x, double y) {
+    this.dartPoint = new Point(x, y);
+    this._score = calculateScore(dartPoint);
+  }
+
+  int score() {
+    return _score;
+  }
+
+  static int calculateScore(Point dartPoint) {
+    double dartDistance = center.distanceTo(dartPoint);
+    if (dartDistance <= innerRadius) {
+      return innerPoints;
+    } else if (dartDistance <= middleRadius) {
+      return middlePoints;
+    } else if (dartDistance <= outerRadius) {
+      return outerPoints;
+    } else {
+      return noTargetPoints;
+    }
+  }
+
+  public static class Point {
+    private double x, y;
+
+    public Point(double x, double y) {
+      this.x = x;
+      this.y = y;
     }
 
-    int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public double distanceTo(Point other) {
+      double dx = this.x - other.x;
+      double dy = this.y - other.y;
+      return Math.sqrt(dx * dx + dy * dy);
     }
-
+  }
 }
