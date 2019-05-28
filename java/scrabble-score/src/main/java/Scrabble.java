bd64b4c7b36274fdec08f4a8b9de21757ec4aff1
Harrison McCullough <mccullough.harrison@gmail.com>
2019-05-28 13:10:28 -0600

    Update java/scrabble to use switch statement instead of Map

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,46 +1,9 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Scrabble {
 
-  private String word;
   private int score;
 
-  static final Map<Character, Integer> letterToScore;
-
-  static {
-    letterToScore = new HashMap<Character, Integer>();
-    letterToScore.put('A', 1);
-    letterToScore.put('E', 1);
-    letterToScore.put('I', 1);
-    letterToScore.put('O', 1);
-    letterToScore.put('U', 1);
-    letterToScore.put('L', 1);
-    letterToScore.put('N', 1);
-    letterToScore.put('R', 1);
-    letterToScore.put('S', 1);
-    letterToScore.put('T', 1);
-    letterToScore.put('D', 2);
-    letterToScore.put('G', 2);
-    letterToScore.put('B', 3);
-    letterToScore.put('C', 3);
-    letterToScore.put('M', 3);
-    letterToScore.put('P', 3);
-    letterToScore.put('F', 4);
-    letterToScore.put('H', 4);
-    letterToScore.put('V', 4);
-    letterToScore.put('W', 4);
-    letterToScore.put('Y', 4);
-    letterToScore.put('K', 5);
-    letterToScore.put('J', 8);
-    letterToScore.put('X', 8);
-    letterToScore.put('Q', 10);
-    letterToScore.put('Z', 10);
-  }
-
   Scrabble(String word) {
-    this.word = word;
-    this.score = getScore(this.word);
+    this.score = getScore(word);
   }
 
   int getScore() {
@@ -51,9 +14,68 @@ class Scrabble {
     word = word.toUpperCase();
     int score = 0;
     for (int i = 0; i < word.length(); i++) {
-      char c = word.charAt(i);
-      score += letterToScore.get(c);
+      char letter = word.charAt(i);
+      score += getLetterScore(letter);
     }
     return score;
+  }
+
+  static int getLetterScore(char letter) {
+    switch (letter) {
+      case 'A':
+        return 1;
+      case 'E':
+        return 1;
+      case 'I':
+        return 1;
+      case 'O':
+        return 1;
+      case 'U':
+        return 1;
+      case 'L':
+        return 1;
+      case 'N':
+        return 1;
+      case 'R':
+        return 1;
+      case 'S':
+        return 1;
+      case 'T':
+        return 1;
+      case 'D':
+        return 2;
+      case 'G':
+        return 2;
+      case 'B':
+        return 3;
+      case 'C':
+        return 3;
+      case 'M':
+        return 3;
+      case 'P':
+        return 3;
+      case 'F':
+        return 4;
+      case 'H':
+        return 4;
+      case 'V':
+        return 4;
+      case 'W':
+        return 4;
+      case 'Y':
+        return 4;
+      case 'K':
+        return 5;
+      case 'J':
+        return 8;
+      case 'X':
+        return 8;
+      case 'Q':
+        return 10;
+      case 'Z':
+        return 10;
+      default:
+        throw new IllegalArgumentException("Illegal letter " + letter);
+    }
   }
 }
