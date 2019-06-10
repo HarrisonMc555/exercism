7afaf2176cddd2bc91747704e85824658b339a24
Harrison McCullough <mccullough.harrison@gmail.com>
2019-06-10 12:55:31 -0600

    Use sumBy and multiple keys to value in map for ktolin/scrabble

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,36 +1,17 @@
 object ScrabbleScore {
     fun scoreWord(input: String): Int {
-        return input.map { scoreLetter(it) }.sum()
+        return input.sumBy { scoreLetter(it) }
     }
-
+    
     fun scoreLetter(input: Char): Int {
         return when (input.toUpperCase()) {
-            'A' -> 1
-            'E' -> 1
-            'I' -> 1
-            'O' -> 1
-            'U' -> 1
-            'L' -> 1
-            'N' -> 1
-            'R' -> 1
-            'S' -> 1
-            'T' -> 1
-            'D' -> 2
-            'G' -> 2
-            'B' -> 3
-            'C' -> 3
-            'M' -> 3
-            'P' -> 3
-            'F' -> 4
-            'H' -> 4
-            'V' -> 4
-            'W' -> 4
-            'Y' -> 4
+            'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' -> 1
+            'D', 'G' -> 2
+            'B', 'C', 'M', 'P' -> 3
+            'F', 'H', 'V', 'W', 'Y' -> 4
             'K' -> 5
-            'J' -> 8
-            'X' -> 8
-            'Q' -> 10
-            'Z' -> 10
+            'J', 'X' -> 8
+            'Q', 'Z' -> 10
             else -> throw IllegalArgumentException("Invalid letter $input")
         }
     }
