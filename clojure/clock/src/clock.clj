e5e58442ed1b609fbc87b6286ab6a4cadb3370b2
Harrison McCullough <mccullough.harrison@gmail.com>
2019-06-27 09:02:21 -0600

    Finish clojure/clock

--- clojure/clock/src/clock.clj
+++ clojure/clock/src/clock.clj
@@ -2,11 +2,25 @@
 
 (defrecord Clock [hours minutes])
 
-(defn clock->string [clock]
-  (format "%02d:%02d" (:hours clock) (:minutes clock)))
+(def ^:private ^:const MINUTES_IN_HOUR 60)
+(def ^:private ^:const HOURS_IN_DAY 24)
+(def ^:private ^:const MINUTES_IN_DAY (* MINUTES_IN_HOUR HOURS_IN_DAY))
 
-(defn clock [hours minutes]
-  (Clock. hours minutes))
+(defn clock
+  ([hours minutes]
+   (let [total-minutes (+ (* hours MINUTES_IN_HOUR) minutes)
+         total-minutes-wrapped (mod total-minutes MINUTES_IN_DAY)
+         new-hours (quot total-minutes-wrapped MINUTES_IN_HOUR)
+         new-minutes (mod total-minutes-wrapped MINUTES_IN_HOUR)]
+     (Clock. new-hours new-minutes)))
+  ([minutes]
+   (clock 0 minutes)))
+
+(defn- clock-total-minutes [clock]
+  (+ (* MINUTES_IN_HOUR (:hours clock)) (:minutes clock)))
 
 (defn add-time [clock minutes]
-  (update clock :minutes + minutes))
+  (clock/clock (+ (clock-total-minutes clock) minutes)))
+
+(defn clock->string [clock]
+  (format "%02d:%02d" (:hours clock) (:minutes clock)))
