e6d00bd93f68dce4851e963adce4f34b1e726de7
Harrison McCullough <mccullough.harrison@gmail.com>
2019-07-21 17:58:51 -0600

    Fix haskell/bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -3,10 +3,12 @@ module Bob (responseFor) where
 import Data.Char
 
 responseFor :: String -> String
-responseFor input | isSilence  input = "Fine. Be that way!"
-                  | isYelling  input = "Whoa, chill out!"
-                  | isQuestion input = "Sure."
-                  | otherwise        = "Whatever."
+responseFor input
+  | isSilence  input = "Fine. Be that way!"
+  | isYelling input && isQuestion input = "Calm down, I know what I'm doing!"
+  | isYelling  input = "Whoa, chill out!"
+  | isQuestion input = "Sure."
+  | otherwise        = "Whatever."
 
 isYelling :: String -> Bool
 isYelling input = any isAlpha input && input == map toUpper input
