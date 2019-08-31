ce8b187b9b0add5430880b217e03351405a940a8
Harrison McCullough <mccullough.harrison@gmail.com>
2019-08-31 00:53:57 -0600

    Finish clojure/reverse-string

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,9 +1,22 @@
-(ns reverse-string
-  (:require [clojure.string :as str]))
+(ns reverse-string)
+
+(defn- my-rev [vector]
+  (let [indices (range (dec (count vector)) -1 -1)]
+    (mapv #(nth vector %) indices)))
 
 (defn reverse-string [s]
-  (if (= s "")
-    ""
-    (let [first (get s 0)
-          rest (subs s 1)]
-      (str (reverse-string rest) first))))
+  (let [forward-vec (char-array s)
+        reverse-vec (my-rev forward-vec)]
+    (apply str reverse-vec)))
+
+(defn- rev-accum [forward reversed]
+  (let [x (first forward)]
+    (if (nil? x)
+      reversed
+      (rev-accum (rest forward) (cons x reversed)))))
+
+(defn- my-rev2 [sq]
+  (apply str (rev-accum sq ())))
+
+(defn- reverse-string2 [s]
+  (apply str (my-rev2 s)))
