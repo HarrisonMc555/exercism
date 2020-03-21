132f3d2f339dafe96321379ac00b9620226bc838
Harrison McCullough <mccullough.harrison@gmail.com>
2020-03-21 10:16:58 -0600

    Finish ruby/high-scores

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Keep track of a user's high scores
+class HighScores
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    @scores.last
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.max(3)
+  end
+
+  def latest_is_personal_best?
+    latest == personal_best
+  end
+end
