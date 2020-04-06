e82a09123447233a5619a18084a091c048c122d1
Harrison McCullough <mccullough.harrison@gmail.com>
2020-04-05 18:13:00 -0600

    Finish javascript/resistor-color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,18 @@
+export const colorCode = (color) => {
+  return COLOR_TO_NUM[color];
+};
+
+const COLOR_TO_NUM = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export const COLORS = Object.keys(COLOR_TO_NUM);
