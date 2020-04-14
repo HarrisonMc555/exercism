ef42876f064406ac6627c52399a155c6206426df
Harrison McCullough <mccullough.harrison@gmail.com>
2020-04-14 07:52:32 -0600

    Update c/armstrong-numbers

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -3,7 +3,7 @@
 
 #define BASE 10
 
-int pow_int(const int base, const int power) {
+static int pow_int(const int base, const int power) {
     int result = 1;
     for (int i = 0; i < power; i++) {
         result *= base;
@@ -14,11 +14,10 @@ int pow_int(const int base, const int power) {
 int is_armstrong_number(const int candidate) {
     int candidate_copy = candidate;
     int sum = 0;
-    int num_digits = log10(candidate) + 1;
+    const int num_digits = log10(candidate) + 1;
     while (candidate_copy > 0) {
-        int digit = candidate_copy % BASE;
-        int power = num_digits;
-        sum += pow_int(digit, power);
+        const int digit = candidate_copy % BASE;
+        sum += pow_int(digit, num_digits);
         candidate_copy /= BASE;
     }
     return candidate == sum;
