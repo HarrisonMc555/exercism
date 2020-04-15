2ddd54b7e1d5b83483321acf0f90614e247cfb11
Harrison McCullough <mccullough.harrison@gmail.com>
2020-04-15 11:46:14 -0600

    Update ruby/matrix

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -3,18 +3,22 @@
 # Store a 2D matrix
 class Matrix
   attr_reader :rows
-  attr_reader :columns
 
   def initialize(string)
-    @rows = Matrix.parse(string)
-    @columns = @rows.transpose
+    @rows = parse(string)
   end
 
-  def self.parse(string)
-    string.split(/\n+/).map(&Matrix.method(:parse_line))
+  def columns
+    @rows.transpose
   end
 
-  def self.parse_line(line)
+  private
+
+  def parse(string)
+    string.split(/\n+/).map(&method(:parse_line))
+  end
+
+  def parse_line(line)
     line.split.map(&:to_i)
   end
 end
