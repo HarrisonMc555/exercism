3e2ce3569d35edbea999f2a746e18674d80093f6
Harrison McCullough <mccullough.harrison@gmail.com>
2020-04-16 08:30:34 -0600

    Finish ruby/scrabble-score

---
+++ ruby/scrabble-score/scrabble_score.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# Score a Scrabble word
+class Scrabble
+  LETTER_TO_SCORE = {
+    'A' => 1,
+    'E' => 1,
+    'I' => 1,
+    'O' => 1,
+    'U' => 1,
+    'L' => 1,
+    'N' => 1,
+    'R' => 1,
+    'S' => 1,
+    'T' => 1,
+    'D' => 2,
+    'G' => 2,
+    'B' => 3,
+    'C' => 3,
+    'M' => 3,
+    'P' => 3,
+    'F' => 4,
+    'H' => 4,
+    'V' => 4,
+    'W' => 4,
+    'Y' => 4,
+    'K' => 5,
+    'J' => 8,
+    'X' => 8,
+    'Q' => 10,
+    'Z' => 10,
+  }.freeze
+
+  def initialize(word)
+    @word = (word || '').upcase
+  end
+
+  def score
+    @word.split('').map { |letter| LETTER_TO_SCORE[letter] || 0 }.sum
+  end
+
+  def self.score(word)
+    Scrabble.new(word).score
+  end
+end
