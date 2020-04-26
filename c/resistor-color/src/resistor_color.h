8f5f63063e369e7267e144636a7d5bdfb50f2c10
Harrison McCullough <mccullough.harrison@gmail.com>
2020-04-26 11:50:25 -0600

    Use enum in c/resistor-color

--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -1,18 +1,18 @@
 #ifndef RESISTOR_COLOR_H
 #define RESISTOR_COLOR_H
 
-typedef unsigned int resistor_band_t;
-
-#define BLACK 0
-#define BROWN 1
-#define RED 2
-#define ORANGE 3
-#define YELLOW 4
-#define GREEN 5
-#define BLUE 6
-#define VIOLET 7
-#define GREY 8
-#define WHITE 9
+typedef enum resistor_band_t {
+    BLACK = 0,
+    BROWN = 1,
+    RED = 2,
+    ORANGE = 3,
+    YELLOW = 4,
+    GREEN = 5,
+    BLUE = 6,
+    VIOLET = 7,
+    GREY = 8,
+    WHITE = 9,
+} resistor_band_t;
 
 resistor_band_t* colors(void);
 
