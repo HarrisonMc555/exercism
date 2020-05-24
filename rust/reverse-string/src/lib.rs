939912eb664925bf8e0eaedfc70a1e7bd7bb7de2
Harrison McCullough <mccullough.harrison@gmail.com>
2020-05-23 22:18:47 -0600

    Update rust/reverse-string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,5 @@
-extern crate unicode_segmentation;
-
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    UnicodeSegmentation::graphemes(input, true).rev().collect::<String>()
+    input.graphemes(true).rev().collect()
 }
