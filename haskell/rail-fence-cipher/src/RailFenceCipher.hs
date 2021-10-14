e86df8ee7064d0e55696cd58f2c96abddf8f3bf7
Harrison McCullough <mccullough.harrison@gmail.com>
2021-10-14 11:28:55 -0600

    Start work on haskell/rail-fence-cipher

--- haskell/rail-fence-cipher/src/RailFenceCipher.hs
+++ haskell/rail-fence-cipher/src/RailFenceCipher.hs
@@ -1,7 +1,19 @@
 module RailFenceCipher (encode, decode) where
 
+import Data.Vector (Vector, fromList)
+
+data Direction = Down | Up
+
 encode :: Int -> String -> String
-encode = error "You need to implement this function!"
+encode x s = s
 
 decode :: Int -> String -> String
 decode = error "You need to implement this function!"
+
+toRails :: Int -> String -> Vector (Vector Char)
+toRails _ _ = fromList []
+-- toRails num = go 0 Down []
+--   where go _ _ acc "" = acc
+--         go i dir (c:cs) =
+--           case (i, dir) of
+--             (num - 1, Down) -> go (i - 1) Up []
