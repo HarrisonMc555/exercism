a227de7be5953b9f74301ca03ad5bc77a403b194
Harrison McCullough <mccullough.harrison@gmail.com>
2022-11-23 12:56:54 -0700

    Update typescript/leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,4 +1,4 @@
-function isLeapYear(year: number): boolean {
+export function isLeap(year: number): boolean {
     const divisbleBy4 = isDivisibleBy(year, 4)
     const divisbleBy100 = isDivisibleBy(year, 100)
     const divisbleBy400 = isDivisibleBy(year, 400)
@@ -10,5 +10,3 @@ const ERROR_MARGIN = 0.000000001
 function isDivisibleBy(num: number, divisor: number): boolean {
     return Math.abs(num % divisor) < ERROR_MARGIN
 }
-
-export default isLeapYear
