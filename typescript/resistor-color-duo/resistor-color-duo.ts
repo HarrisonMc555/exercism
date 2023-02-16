31954791b2b5ca86bb87bec4c9a9b6f24c529d09
Harrison McCullough <mccullough.harrison@gmail.com>
2023-02-16 12:10:30 -0700

    Update typescript/resistor-color-duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -12,27 +12,9 @@ const COLOR_TO_DIGIT: Record<string, number> = {
 };
 
 type Color = keyof typeof COLOR_TO_DIGIT;
+const BASE = 10;
 
-export class ResistorColor {
-  private colors: Color[];
-
-  constructor(colors: Color[]) {
-    if (colors.length < 2) {
-      throw "At least two colors need to be present";
-    }
-    this.colors = colors.slice(0, 2);
-  }
-
-  value = (): number => {
-    let num = 0;
-    for (const color of this.colors) {
-      num *= 10;
-      num += ResistorColor.colorToDigit(color);
-    }
-    return num;
-  };
-
-  private static colorToDigit(color: Color): number {
-    return COLOR_TO_DIGIT[color];
-  }
+export function decodedValue(colors: [Color, Color, ...Color[]]): number {
+  let [color1, color2] = colors;
+  return BASE * COLOR_TO_DIGIT[color1] + COLOR_TO_DIGIT[color2];
 }
