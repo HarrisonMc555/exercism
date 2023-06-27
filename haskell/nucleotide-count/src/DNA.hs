320ac7b0688e3c77329a3b99075b257b732e0253
Harrison McCullough <mccullough.harrison@gmail.com>
2023-06-26 23:33:06 -0700

    Update and fix haskell/nucleotide-count

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,18 +1,27 @@
-module DNA (nucleotideCounts) where
+module DNA (nucleotideCounts, Nucleotide(..)) where
 import Data.Map (Map, fromList, alter, member)
 
-nucleotides :: String
-nucleotides = "AGCT"
+data Nucleotide = A | G | C | T deriving (Eq, Ord, Show)
 
-emptyNucleotideMap :: Map Char Int
+nucleotides :: [Nucleotide]
+nucleotides = [A, G, C, T]
+
+emptyNucleotideMap :: Map Nucleotide Int
 emptyNucleotideMap = Data.Map.fromList $ map empty nucleotides
   where empty c = (c, 0)
 
-nucleotideCounts :: String -> Either String (Map Char Int)
+nucleotideCounts :: String -> Either String (Map Nucleotide Int)
 nucleotideCounts = nucleotideCountsAccum emptyNucleotideMap
   where nucleotideCountsAccum m "" = Right m
-        nucleotideCountsAccum m (c:cs)
-          | member c emptyNucleotideMap = nucleotideCountsAccum m' cs
-          | otherwise                   = Left $ "Invalid nucleotide " ++ [c]
-          where m'  = alter inc c m
-                inc = fmap (1+)
+        nucleotideCountsAccum m (c:cs) =
+          case fromChar c of
+            Just n -> let m' = alter (fmap (+1)) n m
+                      in nucleotideCountsAccum m' cs
+            Nothing -> Left $ "Invalid nucleotide " ++ [c]
+
+fromChar :: Char -> Maybe Nucleotide
+fromChar 'A' = Just A
+fromChar 'G' = Just G
+fromChar 'C' = Just C
+fromChar 'T' = Just T
+fromChar _ = Nothing
