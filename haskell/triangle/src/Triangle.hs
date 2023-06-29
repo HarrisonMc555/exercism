626a534f7061d32ec0ffa478bb985b9afb195ad0
Harrison McCullough <mccullough.harrison@gmail.com>
2023-06-29 07:38:44 -0700

    Update and fix haskell/triangle

--- haskell/triangle/src/Triangle.hs
+++ haskell/triangle/src/Triangle.hs
@@ -14,4 +14,10 @@ triangleType a b c
   | x == y && y == z = Equilateral
   | x == y || y == z = Isosceles
   | otherwise        = Scalene
-  where [x,y,z] = sort [a,b,c]
+  where (x, y, z) = sort3 (a, b, c)
+
+sort3 :: (Num a, Ord a) => (a, a, a) -> (a, a, a)
+sort3 (a, b, c) =
+  case sort [a, b, c] of
+    [x, y, z] -> (x, y, z)
+    _ -> error "unreachable"
