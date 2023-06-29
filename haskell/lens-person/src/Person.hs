856915427fadcfc3d881d0b049e479ed0ae99388
Harrison McCullough <mccullough.harrison@gmail.com>
2023-06-28 23:44:46 -0700

    Update and fix haskell/lens-person

--- haskell/lens-person/src/Person.hs
+++ haskell/lens-person/src/Person.hs
@@ -12,8 +12,9 @@ module Person
   ) where
 
 import Control.Lens (makeLenses, (^.), (.~), over)
-import Data.Time.Calendar (Day)
-import Data.Time.Lens (months)
+import Control.Lens.Type (Lens')
+import Data.Time.Calendar (Day, toGregorian, fromGregorian)
+-- import Data.Time.Lens (months)
 
 data Person = Person { _name    :: Name
                      , _born    :: Born
@@ -54,3 +55,16 @@ setBirthMonth newMonth = born.bornOn.months .~ newMonth
 
 renameStreets :: (String -> String) -> Person -> Person
 renameStreets f = over (address.street) f . over (born.bornAt.street) f
+
+months :: Lens' Day Int
+months =
+  lens getMonth setMonth
+  where getMonth day =
+          let (_, m, _) = toGregorian day
+          in m
+        setMonth day m =
+          let (y, _, d) = toGregorian day
+          in fromGregorian y m d
+
+lens :: (Functor f) => (s -> a) -> (s -> a -> s) -> (a -> f a) -> s -> f s
+lens getter setter f s = setter s <$> f (getter s)
