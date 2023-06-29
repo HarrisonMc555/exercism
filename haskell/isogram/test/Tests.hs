6139d4f5cf3de4de489134cd487ec67b0c3fc508
Harrison McCullough <mccullough.harrison@gmail.com>
2023-06-29 11:16:04 -0700

    Update and fix haskell/isogram

--- haskell/isogram/test/Tests.hs
+++ haskell/isogram/test/Tests.hs
@@ -1,13 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 import Data.Foldable     (for_)
+import Data.String       (fromString)
 import Test.Hspec        (Spec, describe, it, shouldBe)
-import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
+import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
 
 import Isogram (isIsogram)
 
 main :: IO ()
-main = hspecWith defaultConfig {configFastFail = True} specs
+main = hspecWith defaultConfig {configFailFast = True} specs
 
 specs :: Spec
 specs = describe "isIsogram" $ for_ cases test
@@ -15,8 +17,7 @@ specs = describe "isIsogram" $ for_ cases test
 
     test Case{..} = it description assertion
       where
-        assertion = isIsogram input `shouldBe` expected
-
+        assertion = isIsogram (fromString input) `shouldBe` expected
 
 data Case = Case { description :: String
                  , input       :: String
@@ -48,9 +49,17 @@ cases = [ Case { description = "empty string"
                , input       = "Alphabet"
                , expected    = False
                }
+        , Case { description = "word with duplicated character in mixed case, lowercase first"
+               , input       = "isalpAbet"
+               , expected    = False
+               }
         , Case { description = "hypothetical isogrammic word with hyphen"
                , input       = "thumbscrew-japingly"
                , expected    = True
+               }
+        , Case { description = "hypothetical word with duplicated character following hyphen"
+               , input       = "thumbscrew-jappingly"
+               , expected    = False
                }
         , Case { description = "isogram with duplicated hyphen"
                , input       = "six-year-old"
@@ -62,6 +71,10 @@ cases = [ Case { description = "empty string"
                }
         , Case { description = "duplicated character in the middle"
                , input       = "accentor"
+               , expected    = False
+               }
+        , Case { description = "same first and last characters"
+               , input       = "angola"
                , expected    = False
                }
         ]
