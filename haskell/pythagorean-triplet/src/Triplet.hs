4ffd86de3d9880e4da3fcef88a4f7e7d61532e77
Harrison McCullough <mccullough.harrison@gmail.com>
2023-06-28 21:31:27 -0700

    Update and fix haskell/pythagorean-triplet

--- haskell/pythagorean-triplet/src/Triplet.hs
+++ haskell/pythagorean-triplet/src/Triplet.hs
@@ -1,36 +1,13 @@
-module Triplet (isPythagorean, mkTriplet, pythagoreanTriplets) where
+module Triplet (tripletsWithSum) where
 
-isPythagorean :: (Int, Int, Int) -> Bool
-isPythagorean triplet = square x + square y == square z
-  where (x, y, z) = orderedTriplet triplet
-
-mkTriplet :: Int -> Int -> Int -> (Int, Int, Int)
-mkTriplet a b c = (a, b, c)
-
-pythagoreanTriplets :: Int -> Int -> [(Int, Int, Int)]
-pythagoreanTriplets minFactor maxFactor = 
-  [ mkTriplet a b c
-  | a <- [minFactor.. maxFactor]
-  , b <- [a.. maxFactor]
-  , let c = isqrt $ square a + square b
-          , c <= maxFactor
-          , isPythagorean $ mkTriplet a b c
+tripletsWithSum :: Int -> [(Int, Int, Int)]
+tripletsWithSum target =
+  [ (a, b, c)
+  | a <- [1..target]
+  , b <- [(a + 1)..(target - a)]
+  , let c = target - a - b
+  , square a + square b == square c
   ]
-
-orderedTriplet :: (Int, Int, Int) -> (Int, Int, Int)
-orderedTriplet (a, b, c)
-  | y <= c    = (x, y, c)
-  | x <= c    = (x, c, y)
-  | otherwise = (c, x, y)
-  where (x, y) = orderedPair (a, b)
-
-orderedPair :: (Int, Int) -> (Int, Int)
-orderedPair (a, b)
-  | a <= b    = (a, b)
-  | otherwise = (b, a)
-
-isqrt :: Int -> Int
-isqrt x = floor . sqrt $ (fromIntegral x :: Float)
 
 square :: Int -> Int
 square x = x ^ (2 :: Int)
